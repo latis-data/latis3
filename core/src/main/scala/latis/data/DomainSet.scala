d94567c0edfddffd535a72709f3338be053e6409
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-04-05 11:48:29 -0600

    Provide a default implementation of a DomainSet

--- core/src/main/scala/latis/data/DomainSet.scala
+++ core/src/main/scala/latis/data/DomainSet.scala
@@ -4,3 +4,10 @@ trait DomainSet {
   
   def elements: Seq[DomainData]
 }
+
+object DomainSet {
+  
+  def apply(_elements: Seq[DomainData]) = new DomainSet {
+    def elements: Seq[DomainData] = _elements
+  }
+}
