68c31fe4ca54ff3bd5e1c02262c71adbc801f7fb
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-05-15 15:52:44 -0600

    Define Sample as a type with methods added via SampleOps

--- core/src/main/scala/latis/data/package.scala
+++ core/src/main/scala/latis/data/package.scala
@@ -13,6 +13,12 @@ package object data {
   type RangeData = Vector[Any]
   
   /**
+   * Define a type alias for a Sample as a pair (scala Tuple2) of
+   * DomainData and RangeData.
+   */
+  type Sample = (DomainData, RangeData)
+  
+  /**
    * Define a SamplePath as a Seq of SamplePositions.
    * Each element in the path implies a nested Function.
    */
@@ -24,6 +30,50 @@ package object data {
    */
   implicit object SampleOrdering extends Ordering[Sample] {
     def compare(a: Sample, b: Sample) = 
-      DomainOrdering.compare(a.domain, b.domain)
+      DomainOrdering.compare(a.domain, b.range)
   }
+  
+  /**
+   * Define implicit class to provide operations on Sample objects.
+   */
+  implicit class SampleOps(sample: Sample) {
+    
+    def domain: DomainData = sample._1
+    
+    def range:  RangeData = sample._2
+    
+    /**
+     * Get the data values from this Sample at the given SamplePosition.
+     * The value could represent a Scalar variable or a nested Function.
+     */
+    def getValue(samplePosition: SamplePosition): Option[Any] = samplePosition match {
+      //check index OOB
+      case DomainPosition(n) =>
+        if (n < domain.length) Some(domain(n))
+        else None
+      case RangePosition(n) =>
+        if (n < range.length) Some(range(n))
+        else None
+    }
+
+    /**
+     * Return a new Sample with the given value in the given position.
+     */
+    def updatedValue(samplePosition: SamplePosition, value: Any): Sample = samplePosition match {
+      //TODO: insert values if Seq[Any]
+      case DomainPosition(n) =>
+        if (n < domain.length) Sample(domain.updated(n, value), range)
+        else ??? //TODO: error
+      case RangePosition(n) =>
+        if (n < range.length) Sample(domain, range.updated(n, value))
+        else ??? //TODO: error
+    }
+    
+//  def zipPositionWithValue: Seq[(SamplePosition, Any)] = {
+//    val d = domain.zipWithIndex.map(p => (DomainPosition(p._2), p._1))
+//    val r = range.zipWithIndex.map(p => (RangePosition(p._2), p._1))
+//    d ++ r
+//  }
+  }
+  
 }
