ff60a85a354e4ad3292e08c874b18b5a122c8412
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-06-12 15:03:48 -0600

    Implement Append by constructing a CompositeSampledFunction

--- core/src/main/scala/latis/ops/Append.scala
+++ core/src/main/scala/latis/ops/Append.scala
@@ -1,7 +1,7 @@
 package latis.ops
 
 import latis.model.Dataset
-import latis.data.StreamFunction
+import latis.data._
 
 /**
  * Join two Datasets by appending their Streams of Samples.
@@ -10,9 +10,10 @@ case class Append() extends BinaryOperation {
   
   def apply(ds1: Dataset, ds2: Dataset): Dataset = (ds1, ds2) match {
     case (Dataset(md1, model1, data1),Dataset(md2, model2, data2)) =>
-      //TODO: deal with metadata, prov
+      //TODO: deal with metadata, prov; keep orig for now
       //TODO: assert that model1 = model2
-      val data = StreamFunction(data1.streamSamples ++ data1.streamSamples)
+      
+      val data = CompositeSampledFunction(ds1.data, ds2.data)
       Dataset(md1, model1, data)
   }
 }
