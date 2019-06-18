0dbc0e38c904f8dd41c44e680e45e66362c2f114
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-06-18 12:56:44 -0600

    Change GranuleListJoin to make a CompositeSampledFunction

--- core/src/test/scala/latis/input/TestGranuleListJoin.scala
+++ core/src/test/scala/latis/input/TestGranuleListJoin.scala
@@ -11,11 +11,14 @@ import latis.output._
 import java.io.FileOutputStream
 
 import org.junit._
+import org.junit.Assert._
+import latis.ops.Selection
+import latis.ops.Projection
 
 class TestGranuleListJoin {
   //TODO: ScalaTest flat spec?
   
-  //@Test
+  @Test
   def test {
     //granule list dataset: i -> uri
     val gl: Dataset = {
@@ -49,9 +52,30 @@ class TestGranuleListJoin {
     val glj = GranuleListJoin(model, config)
     //val glj = GranuleListJoin(model, adapter)
     
+    val ops = Seq(
+      Selection("a", ">=", "2"),
+      Selection("a", "<=", "3"),
+      //Projection("a,b,d") //TODO: projection not working
+    )
     
-    val ds = glj(gl)
-    val out = System.out //new FileOutputStream("/data/tmp/data3.txt")
-    TextWriter(out).write(ds)
+    val ds = ops.foldLeft(glj(gl))((ds, op) => op(ds))
+    //val out = System.out //new FileOutputStream("/data/tmp/data3.txt")
+    //TextWriter(out).write(ds)
+    val samples = ds.data.unsafeForce.samples
+    assertEquals(2, samples.length)
+    samples(0) match {
+      case Sample(DomainData(a: Short), RangeData(b: Int, c: Float, d: String)) =>
+        assertEquals(2, a)
+        assertEquals(4, b)
+        assertEquals(3.3f, c, 0)
+        assertEquals("c", d)
+    }
+    samples(1) match {
+      case Sample(DomainData(a: Short), RangeData(b: Int, c: Float, d: String)) =>
+        assertEquals(3, a)
+        assertEquals(6, b)
+        assertEquals(4.4f, c, 0)
+        assertEquals("d", d)
+    }
   }
 }
