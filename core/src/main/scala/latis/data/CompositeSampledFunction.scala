cf59e4ad8b8d9b901afab57f1444b2e218cde0a3
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-06-18 14:57:33 -0600

    Delegate some of the functional algebra to component SampledFunctions

--- core/src/main/scala/latis/data/CompositeSampledFunction.scala
+++ core/src/main/scala/latis/data/CompositeSampledFunction.scala
@@ -15,12 +15,42 @@ case class CompositeSampledFunction(
 ) extends SampledFunction {
   //TODO: flatten so we don't end up with nested CSFs?
   
+  /**
+   * Stream Samples by simply concatenating Samples from the component
+   * SampledFunctions.
+   */
   def streamSamples: Stream[IO, Sample] =
     sampledFunctions.map(_.streamSamples).fold(Stream.empty)(_ ++ _)
     
+  /**
+   * A CompositeSampledFunction is empty if it has no component
+   * SampledFunctions or each component SampledFunction is empty.
+   */
   def isEmpty: Boolean = sampledFunctions.exists(! _.isEmpty)
+  
+  /**
+   * Override filter by delegating the predicate application
+   * to each component SampledFunction.
+   */
+  override def filter(p: Sample => Boolean): SampledFunction = 
+    CompositeSampledFunction(sampledFunctions.map(_.filter(p)))
     
-  //TODO: optimize other operations by delegating to granules
+  /**
+   * Override map by delegating the function application
+   * to each component SampledFunction.
+   */
+  override def map(f: Sample => Sample): SampledFunction =
+    CompositeSampledFunction(sampledFunctions.map(_.map(f)))
+    
+  /**
+   * Override flatMap by delegating the function application
+   * to each component SampledFunction.
+   */
+  override def flatMap(f: Sample => MemoizedFunction): SampledFunction =
+    CompositeSampledFunction(sampledFunctions.map(_.flatMap(f)))
+    
+  
+  //TODO: optimize other operations by delegating to granules; e.g. select, project
 }
 
 object CompositeSampledFunction {
