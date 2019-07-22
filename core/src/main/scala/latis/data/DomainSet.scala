f9138b28211616e9934645586cf8780608695211
Doug Lindholm <dlindhol@gmail.com>
2019-07-22 10:27:29 -0600

    Add length and indexOf methods to DomainSet trait

--- core/src/main/scala/latis/data/DomainSet.scala
+++ core/src/main/scala/latis/data/DomainSet.scala
@@ -3,6 +3,14 @@ package latis.data
 trait DomainSet {
   
   def elements: Seq[DomainData]
+  
+  def length: Int = elements.length
+  
+  //getShape: Array[Int]
+  
+  //TODO: interpolate?, insertion point
+  def indexOf(data: DomainData): Int = 
+    elements.indexOf(data) //TODO will this work or do we need indexWhere?
 }
 
 object DomainSet {
