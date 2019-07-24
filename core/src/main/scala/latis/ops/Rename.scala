6d2d42c716068007e33ba23edc492c3dfb16cb0d
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-07-24 17:08:42 -0600

    Add a rename operation

---
+++ core/src/main/scala/latis/ops/Rename.scala
@@ -0,0 +1,18 @@
+package latis.ops
+
+import latis.model._
+
+/**
+ * Define an Operation that renames a specific variable within a Dataset.
+ * This only impacts the model.
+ */
+class Rename(origName: String, newName: String) extends UnaryOperation {
+  
+  override def applyToModel(model: DataType): DataType = {
+    model map {
+      case v if (v.id == origName) => v.rename(newName)
+      //TODO: support aliases with hasName
+      case v => v
+    }
+  }
+}
