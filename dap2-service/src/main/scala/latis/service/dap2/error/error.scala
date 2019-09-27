571ed7e9fe3e7aae61a1b211dfc0214cf1c57166
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2019-09-27 11:32:44 -0600

    Flesh out DAP 2 service implementation

---
+++ dap2-service/src/main/scala/latis/service/dap2/error/error.scala
@@ -0,0 +1,7 @@
+package latis.service.dap2.error
+
+sealed trait Dap2Error extends Exception
+final case class DatasetResolutionFailure(msg: String) extends Dap2Error
+final case class ParseFailure(msg: String) extends Dap2Error
+final case class UnknownExtension(msg: String) extends Dap2Error
+final case class UnknownOperation(msg: String) extends Dap2Error
