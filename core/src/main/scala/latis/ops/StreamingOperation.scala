d3b3fe3dfc56b0d05f7521264c54d85b2216534e
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-10-01 17:48:11 -0600

    Add a trait for operations that composed with MappingOperations

---
+++ core/src/main/scala/latis/ops/StreamingOperation.scala
@@ -0,0 +1,17 @@
+package latis.ops
+
+
+/**
+ * Operations that implement the StreamingOperation trait
+ * can be applied one Sample at a time. These Operations
+ * can thus be composed with a MappingOperation (applied first)
+ */
+trait StreamingOperation {
+  /*
+   * TODO: "streaming" might not be the right abstraction for composition
+   * e.g. groupBy can stream in but not out
+   */
+  
+  //TODO: return this.type?
+  def compose(mappingOp: MappingOperation): StreamingOperation
+}
