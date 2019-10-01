753d3771d00160896a05fecce1e51e967a8ecf78
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-10-01 17:11:23 -0600

    Add a SampledFunction implemented by a SortedMap

---
+++ core/src/main/scala/latis/data/SortedMapFunction.scala
@@ -0,0 +1,21 @@
+package latis.data
+
+import scala.collection.immutable.SortedMap
+
+case class SortedMapFunction(sortedMap: SortedMap[DomainData, RangeData]) extends MemoizedFunction {
+  
+  def samples: Seq[Sample] = sortedMap.toSeq
+  
+  override def apply(value: DomainData): Option[RangeData] = {
+    sortedMap.get(value)
+    //TODO: support interpolation
+  }
+  
+  //TODO: optimize other methods
+}
+
+object SortedMapFunction extends FunctionFactory {
+  
+  def fromSamples(samples: Seq[Sample]): MemoizedFunction =
+    SortedMapFunction(SortedMap(samples: _*))
+}
