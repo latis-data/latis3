5c20411733ec004d2f9d8351d6cbf591e29904a4
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-11-01 17:15:50 -0600

    Improve Selection to use Scalar provided types and Ordering

--- core/src/main/scala/latis/ops/Selection.scala
+++ core/src/main/scala/latis/ops/Selection.scala
@@ -1,38 +1,43 @@
 package latis.ops
 
-import latis.data._
-import latis.metadata._
-import latis.model._
+import latis.data.Data
+import latis.data.Sample
+import latis.data.SampleOps
+import latis.data.SamplePosition
+import latis.model.DataType
+import latis.model.Scalar
 
 /**
  * Operation to keep only Samples that meet the given selection criterion.
  */
-case class Selection(vname: String, operator: String, value: String) extends Filter {
+case class Selection(vname: String, operator: String, value: String)
+    extends Filter {
   //TODO: use smaller types, enumerate operators?
   //TODO: enable IndexedFunction to use binary search...
   //TODO: support nested functions, all or none?
   //TODO: allow value to have units
-  
+
   def makePredicate(model: DataType): Sample => Boolean = {
     // Get the desired Scalar from the model
     //TODO: support aliases
     val scalar: Scalar = model.find(_.id == vname) match {
       case Some(s: Scalar) => s
-      case _ => 
+      case _ =>
         val msg = s"Selection variable not found: $vname"
         throw new UnsupportedOperationException(msg)
     }
-    
+
     // Determine the Sample position of the selected variable
     val pos: SamplePosition = model.getPath(vname) match {
       case Some(p) => p.length match {
         case 1 => p.head
-        case _ => 
+        case _ =>
           val msg = "Selection does not support values in nested Functions."
           throw new UnsupportedOperationException(msg)
       }
+      case None => ??? //shouldn't happen due to earlier check
     }
-    
+
     // Convert selection value to appropriate type for comparison
     val cdata: Data = scalar.convertValue(value)
 
@@ -54,16 +59,15 @@ case class Selection(vname: String, operator: String, value: String) extends Fil
     if (operator == "!=") {
       comparison != 0
     } else {
-      (comparison <  0 && operator.contains("<")) ||
-      (comparison >  0 && operator.contains(">")) ||
+      (comparison < 0 && operator.contains("<")) ||
+      (comparison > 0 && operator.contains(">")) ||
       (comparison == 0 && operator.contains("="))
     }
   }
 }
 
-
 object Selection {
-  
+
   def apply(expression: String): Selection = {
     //TODO: beef up expression parsing
     val ss = expression.split("\\s+") //split on whitespace
