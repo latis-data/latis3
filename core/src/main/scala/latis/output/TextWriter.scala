a1e50746a7b4a7dc20afed658c0a16b20de506d6
Ryan Heins <ryan.heins@lasp.colorado.edu>
2019-11-06 13:56:18 -0700

    Make `TextEncoder` extensible

--- core/src/main/scala/latis/output/TextWriter.scala
+++ core/src/main/scala/latis/output/TextWriter.scala
@@ -7,12 +7,12 @@ import cats.effect.IO
 import fs2._
 
 case class TextWriter(out: OutputStream) {
-  
+
   def write(dataset: Dataset): Unit =
-    TextEncoder.encode(dataset)
-               .through(text.utf8Encode)
-               .through(OutputStreamWriter.unsafeFromOutputStream[IO](out).write)
-               .compile.drain.unsafeRunSync()
+    new TextEncoder().encode(dataset)
+      .through(text.utf8Encode)
+      .through(OutputStreamWriter.unsafeFromOutputStream[IO](out).write)
+      .compile.drain.unsafeRunSync()
 
 }
 
