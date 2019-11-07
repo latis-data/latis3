c099753e60da464351fc2e0be7ffe753d7f59c9f
Ryan Heins <ryan.heins@lasp.colorado.edu>
2019-11-07 16:06:47 -0700

    Add test for `TextEncoder`

--- core/src/test/scala/latis/output/TextEncoderSpec.scala
+++ core/src/test/scala/latis/output/TextEncoderSpec.scala
@@ -3,6 +3,7 @@ package latis.output
 import latis.model.Dataset
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
+import scala.util.Properties.lineSeparator
 
 class TextEncoderSpec extends FlatSpec {
 
@@ -11,9 +12,12 @@ class TextEncoderSpec extends FlatSpec {
    */
   val enc = new TextEncoder
   val ds: Dataset = Dataset.fromName("data")
-  val expectedOutput = List("0 -> (1,1.1,a)",
-                            "1 -> (2,2.2,b)",
-                            "2 -> (4,3.3,c)")
+  val expectedOutput: Seq[String] = List(
+    "a -> (b, c, d)",
+    "0 -> (1,1.1,a)",
+    "1 -> (2,2.2,b)",
+    "2 -> (4,3.3,c)"
+  ).map(_ + lineSeparator)
 
   "A Text encoder" should "encode a dataset to Text" in {
     val encodedList = enc.encode(ds).compile.toList.unsafeRunSync()
