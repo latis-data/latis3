79f6d39fe8d262059af1a22526ba3a1644710218
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-11-21 18:00:59 -0700

    Make sure the DatasetResolver looks in the cache first
    fixes #39

---
+++ core/src/test/scala/latis/input/DatasetResolverSpec.scala
@@ -0,0 +1,50 @@
+package latis.input
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+import latis.metadata.Metadata
+import latis.data._
+import latis.model._
+import latis.dataset._
+import latis.output.TextWriter
+import latis.util.CacheManager
+
+class DatasetResolverSpec extends FlatSpec {
+  
+  val dataset = {
+    val md = Metadata("data")
+    // Note, there is an fdml dataset with the same name
+    // but different structure.
+        
+    val model = Function(
+      Scalar(Metadata("id" -> "myDomain", "type" -> "int")),
+      Scalar(Metadata("id" -> "myRange", "type" -> "int"))
+    )
+    
+    val data = {
+      val samples = List(
+        Sample(DomainData(Data(1)), RangeData(Data(2)))
+      )
+      SampledFunction(samples)
+    }
+
+    new TappedDataset(md, model, data)
+  }
+  
+  dataset.cache()
+  
+  "The DatasetResolver" should "find a cached dataset before all others" in {
+    Dataset.fromName("data").model match {
+      case Function(d, _) =>
+        d.id should be ("myDomain")
+    }
+  }
+  
+  it should "find a dataset if not cached" in {
+    CacheManager.removeDataset("data")
+    Dataset.fromName("data").model match {
+      case Function(d, _) =>
+        d.id should be ("a")
+    }
+  }
+}
