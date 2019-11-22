ea3de22156c3a04fff3ef6e75e907bd841fb3312
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2019-11-22 11:59:47 -0700

    Use CacheManager directly, not as a DatasetResolver
    This will guarantee that Dataset.fromName with always check the cache
    first. Fixes #39

--- core/src/main/scala/latis/input/DatasetResolver.scala
+++ core/src/main/scala/latis/input/DatasetResolver.scala
@@ -7,6 +7,7 @@ import scala.util.Properties
 import scala.util.Try
 
 import latis.dataset.Dataset
+import latis.util.CacheManager
 import latis.util.FileUtils
 import latis.util.LatisConfig
 
@@ -22,7 +23,7 @@ trait DatasetResolver {
   /**
    * Optionally return the Dataset with the given identifier.
    * If the DatasetResolver implementation does not support the given Dataset
-   * return Null. A failure to read the data could result in an empty Dataset
+   * return None. A failure to read the data could result in an empty Dataset
    * as opposed to None.
    */
   def getDataset(id: String): Option[Dataset]
@@ -33,16 +34,22 @@ trait DatasetResolver {
 object DatasetResolver {
   
   /**
-   * Find the DatasetResolver that can provide the requested dataset by name
-   * among the DatasetResolver registered in META-INF/services/ of jar files 
-   * in the classpath.
-   * If the Dataset is not found among any resolver throw a RuntimeException.
+   * Finds a Dataset with the given identifier.
+   * 
+   * If the identified Dataset does not exist in the cache,
+   * this will find the DatasetResolver that can provide it.
+   * If the Dataset is not found among any resolver throw a 
+   * RuntimeException.
    */
   def getDataset(id: String): Dataset = {
+    //TODO: return Option, Either?
+    // could throw ServiceConfigurationError
+    CacheManager.getDataset(id).getOrElse {
     ServiceLoader.load(classOf[DatasetResolver]).asScala
       .flatMap(_.getDataset(id)).headOption.getOrElse {
         throw new RuntimeException(s"Failed to resolve dataset: $id")
       }
+    }
   }
 
 
@@ -50,6 +57,7 @@ object DatasetResolver {
    * Find all datasets and return their IDs.
    */
   def getDatasetIds: Try[Seq[String]] = {
+    //TODO: ask all DatasetResolvers to provide a list/catalog?
     val suffix = ".fdml"
     val searchPathFromConfig =
       LatisConfig.getOrElse("latis.fdml.dir", Properties.userDir)
