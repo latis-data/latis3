41968a796cc1ffe3eb14f8ff3ffe065b492d7ee5
Ryan Heins <ryan.heins@lasp.colorado.edu>
2019-11-22 12:18:55 -0700

    Add unit tests
    Add tests to SampledFunctionSpec and DatasetSpec.

--- core/src/test/scala/latis/data/SampledFunctionSpec.scala
+++ core/src/test/scala/latis/data/SampledFunctionSpec.scala
@@ -6,20 +6,35 @@ import org.scalatest.Matchers._
 
 class SampledFunctionSpec extends FlatSpec {
 
-  val sample1: Sample = Sample(List(0), List(7))
-  val sample2: Sample = Sample(List(1), List(8))
-  val sample3: Sample = Sample(List(2), List(9))
-  val sampledF: SampledFunction = SampledFunction(List(sample1, sample2, sample3))
+
+
   val dataSf: SampledFunction = SampledFunction(Dataset.fromName("data").samples)
 
 
-  "A Sampled Function" should "be able to join another sampled function" in {
+  "A join on a Sampled Function" should "join the range when the domain is the same" in {
+    val sample1: Sample = Sample(List(0), List(7))
+    val sample2: Sample = Sample(List(1), List(8))
+    val sample3: Sample = Sample(List(2), List(9))
+    val sampledF: SampledFunction = SampledFunction(List(sample1, sample2, sample3))
+
     val jointSf: SampledFunction = sampledF.join(dataSf)
     val jointSfList = jointSf.streamSamples.compile.toList.unsafeRunSync()
 
-  jointSfList.length should be(3)
-    jointSfList.map {
-      case Sample(_, r) => r.length should be(4)
+    val expected = List(
+      Sample(DomainData(0), RangeData(7, 1, 1.1, "a")),
+      Sample(DomainData(1), RangeData(8, 2, 2.2, "b")),
+      Sample(DomainData(2), RangeData(9, 4, 3.3, "c")),
+    )
+
+    jointSfList should be(expected)
+  }
+
+  it should "fail if the domains are not the same" in {
+    assertThrows[Exception] {
+      val sampledF2: SampledFunction = SampledFunction(
+        List(Sample(List(42), List(9000))))
+      val failJoin = dataSf.join(sampledF2)
     }
   }
+
 }
