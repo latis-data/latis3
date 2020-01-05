b8f8efbcd0eca73b5bd9bb86277835675e8efb1a
Doug Lindholm <dlindhol@gmail.com>
2020-01-05 08:12:58 -0700

    Add option to get partial or total ordering

--- core/src/test/scala/latis/util/OrderingSpec.scala
+++ core/src/test/scala/latis/util/OrderingSpec.scala
@@ -1,5 +1,7 @@
 package latis.util
 
+import scala.collection.mutable
+
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
@@ -12,25 +14,38 @@ import latis.time.Time
 
 class OrderingSpec extends FlatSpec {
 
-  "2D samples with time" should "be orderable" in {
-    val model = Function(
-      Tuple(
-        Scalar(Metadata("id" -> "x", "type" -> "int")),
-        Time(Metadata("id" -> "time", "type" -> "string", "units" -> "MM/dd/yyyy"))
-      ),
-      Scalar(Metadata("id" -> "x", "type" -> "int"))
-    )
+  val model = Function(
+    Tuple(
+      Scalar(Metadata("id" -> "x", "type" -> "int")),
+      Time(Metadata("id" -> "time", "type" -> "string", "units" -> "MM/dd/yyyy"))
+    ),
+    Scalar(Metadata("id" -> "x", "type" -> "int"))
+  )
 
-    val samples = List(
-      Sample(DomainData(0, "01/01/2001"), RangeData(2)),
-      Sample(DomainData(1, "01/01/2001"), RangeData(4)),
-      Sample(DomainData(1, "02/01/2000"), RangeData(3)),
-      Sample(DomainData(0, "02/01/2000"), RangeData(1)),
-    )
+  val samples = List(
+    Sample(DomainData(0, "01/01/2001"), RangeData(2)),
+    Sample(DomainData(1, "01/01/2001"), RangeData(4)),
+    Sample(DomainData(1, "02/01/2000"), RangeData(3)),
+    Sample(DomainData(0, "02/01/2000"), RangeData(1)),
+  )
+
+  "2D samples with time" should "be sortable" in {
 
     val totalOrdering = LatisOrdering.partialToTotal(LatisOrdering.sampleOrdering(model))
 
     samples.sorted(totalOrdering).map {
+      case Sample(_, RangeData(Integer(x))) => x
+    } should be (List(1,2,3,4))
+  }
+
+  it should "go into a SortedMap ordered by keys" in {
+    val ordering = LatisOrdering.partialToTotal(LatisOrdering.domainOrdering(model.domain.getScalars))
+
+    val smap = mutable.SortedMap[DomainData, RangeData]()(ordering)
+    samples.foreach {
+      case Sample(dd, rd) => smap += (dd -> rd)
+    }
+    smap.map {
       case Sample(_, RangeData(Integer(x))) => x
     } should be (List(1,2,3,4))
   }
