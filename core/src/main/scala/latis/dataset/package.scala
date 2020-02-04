24c32232087074d5d5c5482cbbfe0d5b8bf69708
Doug Lindholm <dlindhol@gmail.com>
2020-02-04 15:11:12 -0700

    Add DatasetOps that provide a DSL for the functional algebra

---
+++ core/src/main/scala/latis/dataset/package.scala
@@ -0,0 +1,19 @@
+package latis
+
+import latis.data.DomainSet
+import latis.ops._
+
+package object dataset {
+
+  implicit class DatasetOps(dataset: Dataset) {
+    def select(exp: String): Dataset = dataset.withOperation(Selection(exp))
+    def project(exp: String): Dataset = dataset.withOperation(Projection(exp))
+    def stride(n: Int): Dataset = dataset.withOperation(Stride(n))
+    def uncurry(): Dataset = dataset.withOperation(Uncurry())
+    def curry(n: Int): Dataset = dataset.withOperation(Curry(n))
+    def groupByVariable(vars: String*): Dataset = dataset.withOperation(GroupByVariable(vars: _*))
+    def groupByBin(set: DomainSet, agg: Aggregation = DefaultAggregation()): Dataset = dataset.withOperation(GroupByBin(set, agg))
+    def substitute(df: DatasetFunction): Dataset = dataset.withOperation(Substitution(df))
+    def compose(df: DatasetFunction): Dataset = dataset.withOperation(Composition(df))
+  }
+}
