cf930ff209bf6d551d4ddf25d19cc72d423fa0f3
Doug Lindholm <dlindhol@gmail.com>
2020-02-04 08:19:25 -0700

    Add toString to DatasetFunction

--- core/src/main/scala/latis/dataset/DatasetFunction.scala
+++ core/src/main/scala/latis/dataset/DatasetFunction.scala
@@ -1,28 +1,30 @@
 package latis.dataset
 
-import latis.data.DomainData
-import latis.data.RangeData
-import latis.data.TupleData
-import latis.metadata.Metadata
-import latis.model.DataType
+import latis.data._
+import latis.metadata._
+import latis.model._
 import latis.util.LatisException
 
-//TODO: ContinuousDataset?
+/**
+ * Defines a Dataset as a computational function.
+ * The function type is described as a DataType
+ * to facilitate composition with Datasets.
+ * The function could be enabled by the evaluation
+ * of a SampledFunction or it could be a pure function.
+ */
 case class DatasetFunction(
   metadata: Metadata,
   model: DataType,
-  //function: DomainData => Either[LatisException, RangeData]
   function: TupleData => Either[LatisException, TupleData]
-) {
-
-  /*
-  TODO: can function domain be any List[Data]?
-    e.g. spectrum => (r, g, b) needs to allow Function
-    also no need for ordering
-    another call for TupleData?
-      then we could have Data => Data
-   */
+) extends MetadataLike {
 
   def apply(data: TupleData): Either[LatisException, TupleData] =
     function(data)
+
+  /**
+   * Uses double arrow (=>) to represent computational function.
+   */
+  override def toString: String = model match {
+    case Function(domain, range) => s"$id: $domain => $range"
+  }
 }
