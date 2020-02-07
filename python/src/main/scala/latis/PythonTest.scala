186bf0e3ce09b8c28d56d23555d83182ed841357
Shawn Polson <sapols@yahoo.com>
2020-02-07 11:54:09 -0700

    Start creating a python subproject

--- python/src/main/scala/latis/PythonTest.scala
+++ python/src/main/scala/latis/PythonTest.scala
@@ -5,17 +5,17 @@ import jep.MainInterpreter
 
 object PythonTest extends App {
   MainInterpreter.setJepLibraryPath("/anaconda3/lib/python3.6/site-packages/jep/jep.cpython-36m-darwin.so")
-  
-	val jep = new Jep()
+
+  val jep = new Jep()
   jep.runScript("python/src/main/python/add.py")
-	val a = 2
-	val b = 3
-	// There are multiple ways to evaluate. Let us demonstrate them:
-	jep.eval(s"c = add($a, $b)")
-	val ans = jep.getValue("c").asInstanceOf[Long]
-	println(ans)
-  
-	//val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
-	//println(ans2)
+  val a = 2
+  val b = 3
+  // There are multiple ways to evaluate. Let us demonstrate them:
+  jep.eval(s"c = add($a, $b)")
+  val ans = jep.getValue("c").asInstanceOf[Long]
+  println(ans)
+
+  //val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
+  //println(ans2)
 
 }
