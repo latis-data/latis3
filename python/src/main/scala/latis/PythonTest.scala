c0fa4f1c37207f2af57362b0a37ab2d74ee39c8c
Shawn Polson <sapols@yahoo.com>
2020-02-08 10:25:22 -0700

    Start trying to use JEP

--- python/src/main/scala/latis/PythonTest.scala
+++ python/src/main/scala/latis/PythonTest.scala
@@ -1,4 +1,17 @@
-// package latis
-//
-// object PythonTest {
-// }
+package latis
+
+import jep.Jep
+
+object PythonTest extends App {
+	val jep = new Jep() //https://github.com/ninia/jep/wiki/FAQ#how-do-i-fix-unsatisfied-link-error-no-jep-in-javalibrarypath
+  jep.runScript("src/main/python/add.py")
+	val a = 2
+	val b = 3
+	// There are multiple ways to evaluate. Let us demonstrate them:
+	jep.eval(s"c = add($a, $b)")
+	val ans = jep.getValue("c").asInstanceOf[Int]
+	println(ans)
+//	val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
+//	println(ans2)
+
+}
