00a69e7c9df53cf8a6dfa5e469758932efc30a96
Shawn Polson <sapols@yahoo.com>
2020-02-10 13:16:21 -0700

    First time getting JEP to work
    (added 2+3 in Python to get 5 in Scala)

--- python/src/main/scala/latis/PythonTest.scala
+++ python/src/main/scala/latis/PythonTest.scala
@@ -1,15 +1,19 @@
 package latis
 
 import jep.Jep
+import jep.MainInterpreter
 
 object PythonTest extends App {
-	val jep = new Jep() //https://github.com/ninia/jep/wiki/FAQ#how-do-i-fix-unsatisfied-link-error-no-jep-in-javalibrarypath
-  jep.runScript("src/main/python/add.py")
+  MainInterpreter.setJepLibraryPath("/anaconda3/lib/python3.6/site-packages/jep/jep.cpython-36m-darwin.so")
+  //System.load("/anaconda3/lib/python3.6/site-packages/jep/jep.cpython-36m-darwin.so")
+  
+	val jep = new Jep()
+  jep.runScript("python/src/main/python/add.py")
 	val a = 2
 	val b = 3
 	// There are multiple ways to evaluate. Let us demonstrate them:
 	jep.eval(s"c = add($a, $b)")
-	val ans = jep.getValue("c").asInstanceOf[Int]
+	val ans = jep.getValue("c").asInstanceOf[Long]
 	println(ans)
 //	val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
 //	println(ans2)
