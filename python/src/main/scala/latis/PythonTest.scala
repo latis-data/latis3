be7ae04ea9b7a0e3698d8c5d007533b82969d9c4
Shawn Polson <sapols@yahoo.com>
2020-02-11 10:07:14 -0700

    Code cleanup

--- python/src/main/scala/latis/PythonTest.scala
+++ python/src/main/scala/latis/PythonTest.scala
@@ -5,7 +5,6 @@ import jep.MainInterpreter
 
 object PythonTest extends App {
   MainInterpreter.setJepLibraryPath("/anaconda3/lib/python3.6/site-packages/jep/jep.cpython-36m-darwin.so")
-  //System.load("/anaconda3/lib/python3.6/site-packages/jep/jep.cpython-36m-darwin.so")
   
 	val jep = new Jep()
   jep.runScript("python/src/main/python/add.py")
@@ -15,7 +14,8 @@ object PythonTest extends App {
 	jep.eval(s"c = add($a, $b)")
 	val ans = jep.getValue("c").asInstanceOf[Long]
 	println(ans)
-//	val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
-//	println(ans2)
+  
+	//val ans2 = jep.invoke("add", a, b).asInstanceOf[Int]
+	//println(ans2)
 
 }
