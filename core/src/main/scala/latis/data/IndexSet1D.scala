07c7dd1c9c11415523231d203afdab680647f08e
Doug Lindholm <dlindhol@gmail.com>
2020-03-10 17:09:39 -0600

    Add linear sets for int values

---
+++ core/src/main/scala/latis/data/IndexSet1D.scala
@@ -0,0 +1,73 @@
+package latis.data
+
+import latis.metadata._
+import latis.model._
+import latis.util.LatisException
+
+//Experimental Int version of LinearSet
+/**
+ * Define a one-dimensional DomainSet in terms of a start value,
+ * increment, and number of samples. Values will be managed as Doubles.
+ * The data values represent regularly spaced "instantaneous" domain data
+ * as opposed to bins.
+ */
+case class IndexSet1D(start: Int, increment: Int, count: Int)
+  extends DomainSet
+  with Serializable {
+  //TODO: require increment > 0 otherwise need to provide alternate ordering
+  //TODO: optional model constructor arg?
+
+  /**
+   * Define the model of this DomainSet assuming double value types
+   * and a 1-based "_#" naming scheme for variable identifiers.
+   */
+  def model: DataType =
+    Scalar(Metadata("id" -> "_1", "type" -> "int"))
+
+  /**
+   * Return the number of values in this DomainSet.
+   * Override to simply return the count.
+   */
+  override def length: Int = count
+
+  override def min: DomainData = DomainData(start)
+  override def max: DomainData = DomainData(start + increment * (count - 1))
+
+  /**
+   * Return the Data values of this DomainSet as an ordered Sequence.
+   */
+  def elements: IndexedSeq[DomainData] = for {
+    index <- (0 until count)
+  } yield DomainData(start + increment * index)
+
+  /**
+   * Get the domain data for the given index.
+   * Override to compute the data value.
+   */
+  override def apply(index: Int): Option[DomainData] =
+    if (isDefinedAt(index)) Some(DomainData(start + increment * index))
+    else None
+
+  /**
+   * Return the index of the matching domain data value.
+   * Return -1 if not contained in this DomainSet.
+   * Override to compute the index.
+   */
+  override def indexOf(data: DomainData): Int = {
+    val index: Int = data match {
+      case DomainData(Index(x)) =>
+        val i = (x - start) / increment
+        // Must be integral for a match to an existing element
+        if (i == Math.floor(i)) i.toInt
+        else -1
+      case _ =>
+        val msg = s"Invalid value for one-dimensional domain IndexSet: $data"
+        throw LatisException(msg)
+    }
+    if (isDefinedAt(index)) index
+    else -1
+  }
+
+}
+
+
---
+++ core/src/main/scala/latis/data/IndexSet2D.scala
@@ -0,0 +1,51 @@
+package latis.data
+
+import latis.model._
+import latis.util.LatisException
+
+//Experimental Int version of LinearSet
+/**
+ * Define a two-dimensional Cartesian DomainSet with regularly spaced elements.
+ * This is implemented as a product of two LinearSet1Ds.
+ */
+case class IndexSet2D(set1: IndexSet1D, set2: IndexSet1D, val model: DataType)
+  extends DomainSet
+  with Serializable {
+  //TODO: ProductSet, could be used for any set of 1D sets
+
+  override def length: Int = set1.length * set2.length
+
+  override def min: DomainData = set1.min ++ set2.min
+  override def max: DomainData = set1.max ++ set2.max
+
+  override def shape: Array[Int] = Array(set1.length, set2.length)
+
+  def elements: IndexedSeq[DomainData] = for {
+    dd1 <- set1.elements
+    dd2 <- set2.elements
+  } yield dd1 ++ dd2
+
+  override def apply(index: Int): Option[DomainData] =
+    if (isDefinedAt(index)) {
+      val i1: Int = index / set2.length
+      val i2: Int = index - (i1 * set2.length)
+      for {
+        dd1 <- set1(i1)
+        dd2 <- set2(i2)
+      } yield dd1 ++ dd2
+    } else None
+
+  override def indexOf(data: DomainData): Int = data match {
+    case DomainData(d1, d2) =>
+      val i1 = set1.indexOf(DomainData(d1))
+      val i2 = set2.indexOf(DomainData(d2))
+      if (set1.isDefinedAt(i1) && set2.isDefinedAt(i2))
+        i1 * set2.length + i2
+      else -1
+    case _ =>
+      val msg = s"Invalid value for two-dimensional domain set: $data"
+      throw LatisException(msg)
+  }
+}
+
+
