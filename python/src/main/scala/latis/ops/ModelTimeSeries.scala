673ad5f8f5d9506e764ef67ec8bbf94e582f9c94
Shawn Polson <sapols@yahoo.com>
2020-04-30 13:35:10 -0600

    Confirm file copy worked

--- python/src/main/scala/latis/ops/ModelTimeSeries.scala
+++ python/src/main/scala/latis/ops/ModelTimeSeries.scala
@@ -119,11 +119,11 @@ trait ModelTimeSeries extends UnaryOperation {
    * Sets the path to the JEP library file if it hasn't already been set.
    */
   private def setJepPath: Unit = try {
-    MainInterpreter.setJepLibraryPath(System.getProperty("user.dir") + "/python/lib/jep.cpython-36m-darwin.so")
+    MainInterpreter.setJepLibraryPath(System.getProperty("user.dir") + "/python/lib/jep.cpython-36m-x86_64-linux-gnu.so")
   } catch {
     case _: JepException => //JEP library path already set
     case _ => try {
-      MainInterpreter.setJepLibraryPath(System.getProperty("user.dir") + "/python/lib/jep.cpython-36m-x86_64-linux-gnu.so")
+      MainInterpreter.setJepLibraryPath(System.getProperty("user.dir") + "/python/lib/jep.cpython-36m-darwin.so")
     } catch {
       case _ => //no-op
     }
