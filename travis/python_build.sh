9119c06bb63e2787b438a5a45fa5d4b07f91fb74
Shawn Polson <sapols@yahoo.com>
2020-05-02 15:45:42 -0600

    Move ls to top of script

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+ls /opt/python/3.6.10/lib/python3.6/site-packages/
 pyenv global 3.6
 wget https://bootstrap.pypa.io/get-pip.py
 python3.6 get-pip.py
 python3.6 -m pip -q install -r python/requirements/requirements.txt
 python3.6 -m pip -q install jep==3.9.0
-ls /opt/python/3.6.10/lib/python3.6/site-packages/
 cp /opt/python/3.6.10/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
