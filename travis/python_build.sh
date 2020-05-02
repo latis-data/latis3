1129d56902772d610b889e248feacbea7e6e4e05
Shawn Polson <sapols@yahoo.com>
2020-05-02 15:36:41 -0600

    Add an ls to see if pip folder is there

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -5,4 +5,5 @@ wget https://bootstrap.pypa.io/get-pip.py
 python3.6 get-pip.py
 python3.6 -m pip -q install -r python/requirements/requirements.txt
 python3.6 -m pip -q install jep==3.9.0
+ls /opt/python/3.6.10/lib/python3.6/site-packages/
 cp /opt/python/3.6.10/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
