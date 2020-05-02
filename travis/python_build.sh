b6d1234c520cad901a1e946c803c0bc86a918c93
Shawn Polson <sapols@yahoo.com>
2020-05-02 15:55:47 -0600

    Check for pip before installing it

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-ls /opt/python/3.6.10/lib/python3.6/site-packages/
 pyenv global 3.6
-wget https://bootstrap.pypa.io/get-pip.py
-python3.6 get-pip.py
+ls /opt/python/3.6.10/lib/python3.6/site-packages/
+if [[ ! -d /opt/python/3.6.10/lib/python3.6/site-packages/pip ]]
+then
+  wget https://bootstrap.pypa.io/get-pip.py
+  python3.6 get-pip.py
+fi
 python3.6 -m pip -q install -r python/requirements/requirements.txt
 python3.6 -m pip -q install jep==3.9.0
 cp /opt/python/3.6.10/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
