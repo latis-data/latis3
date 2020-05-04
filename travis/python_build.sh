b8e9b6cacc1733076e80e4e6a9f32f72d99ed92f
Shawn Polson <sapols@yahoo.com>
2020-05-04 13:17:33 -0600

    Remove ls

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -4,5 +4,4 @@ pyenv global 3.6
 pip -q install -U pip
 pip -q install -r python/requirements/requirements.txt
 pip -q install -r python/requirements/jep.txt
-ls /opt/python/3.6
 cp /opt/python/3.6/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
