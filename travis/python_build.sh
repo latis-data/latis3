6b8c64c9c7063174601b2b992b07e76cc7cca3f0
Shawn Polson <sapols@yahoo.com>
2020-05-04 10:24:03 -0600

    Put jep version in separate file, clean up Travis yaml

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 pyenv global 3.6
-if [[ ! -d /opt/python/3.6.10/lib/python3.6/site-packages/pip ]]
-then
-  wget https://bootstrap.pypa.io/get-pip.py
-  python3.6 get-pip.py
-fi
-pip -q install pip==20.1
-python3.6 -m pip -q install -r python/requirements/requirements.txt
-python3.6 -m pip -q install jep==3.9.0
+pip -q install -U pip
+pip -q install -r python/requirements/requirements.txt
+pip -q install -r python/requirements/jep.txt
 cp /opt/python/3.6.10/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
