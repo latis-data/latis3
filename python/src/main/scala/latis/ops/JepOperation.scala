49e0f0b30571c306b3aa9774bb7196d0fa8eb682
Shawn Polson <sapols@yahoo.com>
2020-05-05 12:54:42 -0600

    Don't redefine parent methods

--- python/src/main/scala/latis/ops/JepOperation.scala
+++ python/src/main/scala/latis/ops/JepOperation.scala
@@ -2,23 +2,11 @@ package latis.ops
 
 import jep.JepException
 import jep.MainInterpreter
-import latis.data.SampledFunction
-import latis.model.DataType
 
 /**
  * Defines an Operation that uses JEP and acts on a single Dataset.
  */
 trait JepOperation extends UnaryOperation {
-
-  /**
-   * Provides a new model resulting from this Operation.
-   */
-  def applyToModel(model: DataType): DataType
-
-  /**
-   * Provides new Data resulting from this Operation.
-   */
-  def applyToData(data: SampledFunction, model: DataType): SampledFunction
 
   /**
    * Sets the path to the JEP library file if it hasn't already been set.
