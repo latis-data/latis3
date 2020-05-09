6300539786d3a87c0b345293f9e88ae751aece3d
Shawn Polson <sapols@yahoo.com>
2020-05-08 21:14:09 -0600

    Print the NaN's class

--- python/src/test/scala/latis/AnomalyDataSpec.scala
+++ python/src/test/scala/latis/AnomalyDataSpec.scala
@@ -58,7 +58,8 @@ class AnomalyDataSpec extends FlatSpec {
       case Sample(DomainData(Number(t)), RangeData(Real(f), Real(a))) =>
         t should be (1)
         f should be (0.841470985)
-        a should be (Float.NaN)
+        println(a.getClass)
+        a should be (Double.NaN)
     }
   }
 
