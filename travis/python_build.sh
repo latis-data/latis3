fe0837d6795c8c13787343e6a051c0de1dd8d292
Shawn Polson <sapols@yahoo.com>
2020-05-13 11:19:05 -0600

    Try using cached site-packages

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 pyenv global 3.6
-pip -q install -U pip
-pip -q install -r python/requirements/requirements.txt
-pip -q install -r python/requirements/jep.txt
-ls $HOME/local/lib/
-#mkdir $HOME/local/lib
-#ls $HOME/local/lib/ 
-ls /opt/python/3.6/lib/python3.6/site-packages/
-cp -r /opt/python/3.6/lib/python3.6/site-packages $HOME/local/lib
-ls $HOME/local/lib
+##pip -q install -U pip
+##pip -q install -r python/requirements/requirements.txt
+##pip -q install -r python/requirements/jep.txt
+#ls $HOME/local/lib/
+#ls /opt/python/3.6/lib/python3.6/site-packages/
+#cp -r /opt/python/3.6/lib/python3.6/site-packages $HOME/local/lib
+#ls $HOME/local/lib
 ls $HOME/local/lib/site-packages
+cp -r $HOME/local/lib/site-packages /opt/python/3.6/lib/python3.6
 cp /opt/python/3.6/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
