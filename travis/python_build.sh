6911dcbf9f1648d586d8aba6f2cd463ae21f6fcc
Shawn Polson <sapols@yahoo.com>
2020-05-13 11:00:30 -0600

    Print $HOME/local/lib/ dir

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -4,5 +4,9 @@ pyenv global 3.6
 pip -q install -U pip
 pip -q install -r python/requirements/requirements.txt
 pip -q install -r python/requirements/jep.txt
+ls $HOME/local/lib/
+#mkdir $HOME/local/lib
+#ls $HOME/local/lib/ 
 ls /opt/python/3.6/lib/python3.6/site-packages/
+#cp /opt/python/3.6/lib/python3.6/site-packages/ 
 cp /opt/python/3.6/lib/python3.6/site-packages/jep/jep.cpython-36m-x86_64-linux-gnu.so /home/travis/build/latis-data/latis3/python/lib/jep.cpython-36m-x86_64-linux-gnu.so
