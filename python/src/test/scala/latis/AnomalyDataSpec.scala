4fde28c3f5cdfe938a424117619df86dccf278df
Shawn Polson <sapols@yahoo.com>
2020-05-14 12:15:41 -0600

    Comment out Text tests

--- python/src/test/scala/latis/AnomalyDataSpec.scala
+++ python/src/test/scala/latis/AnomalyDataSpec.scala
@@ -34,19 +34,19 @@ class AnomalyDataSpec extends FlatSpec {
     }
   }
 
-  it should "work with Text times" in {
-    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
-      .withOperation(ModelWithRollingMean())
-
-    //TextWriter().write(ds)
-
-    StreamUtils.unsafeHead(ds.samples) match {
-      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(rm))) =>
-        t should be ("2000-01-02")
-        f should be (0.841470985)
-        rm should be (0.9432600027000001)
-    }
-  }
+//  it should "work with Text times" in {
+//    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
+//      .withOperation(ModelWithRollingMean())
+//
+//    //TextWriter().write(ds)
+//
+//    StreamUtils.unsafeHead(ds.samples) match {
+//      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(rm))) =>
+//        t should be ("2000-01-02")
+//        f should be (0.841470985)
+//        rm should be (0.9432600027000001)
+//    }
+//  }
 
   "The ARIMA modeling operation" should "add its new column to the dataset" in {
     val ds = Dataset.fromName("sine_wave_with_anomalies")
@@ -62,19 +62,19 @@ class AnomalyDataSpec extends FlatSpec {
     }
   }
 
-  it should "work with Text times" in {
-    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
-      .withOperation(ModelWithArima(0.5, (1,0,0), (0,1,0,50)))
-
-    //TextWriter().write(ds)
-
-    StreamUtils.unsafeHead(ds.samples) match {
-      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(a))) =>
-        t should be ("2000-01-02")
-        f should be (0.841470985)
-        a.isNaN should be (true)
-    }
-  }
+//  it should "work with Text times" in {
+//    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
+//      .withOperation(ModelWithArima(0.5, (1,0,0), (0,1,0,50)))
+//
+//    //TextWriter().write(ds)
+//
+//    StreamUtils.unsafeHead(ds.samples) match {
+//      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(a))) =>
+//        t should be ("2000-01-02")
+//        f should be (0.841470985)
+//        a.isNaN should be (true)
+//    }
+//  }
 
   "The robust random cut forest operation" should "add its new column to the dataset" in {
     val ds = Dataset.fromName("sine_wave_with_anomalies")
@@ -90,19 +90,19 @@ class AnomalyDataSpec extends FlatSpec {
     }
   }
 
-  it should "work with Text times" in {
-    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
-      .withOperation(ScoreWithRRCF())
-
-    //TextWriter().write(ds)
-
-    StreamUtils.unsafeHead(ds.samples) match {
-      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(rrcf))) =>
-        t should be ("2000-01-02")
-        f should be (0.841470985)
-        rrcf should be (0.0)
-    }
-  }
+//  it should "work with Text times" in {
+//    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
+//      .withOperation(ScoreWithRRCF())
+//
+//    //TextWriter().write(ds)
+//
+//    StreamUtils.unsafeHead(ds.samples) match {
+//      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(rrcf))) =>
+//        t should be ("2000-01-02")
+//        f should be (0.841470985)
+//        rrcf should be (0.0)
+//    }
+//  }
   
   "The autoencoder modeling operation" should "add its new column to the dataset" in {
     val ds = Dataset.fromName("sine_wave_with_anomalies")
@@ -118,19 +118,19 @@ class AnomalyDataSpec extends FlatSpec {
     }
   }
 
-  it should "work with Text times" in {
-    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
-      .withOperation(ModelWithAutoencoder())
-
-    //TextWriter().write(ds)
-
-    StreamUtils.unsafeHead(ds.samples) match {
-      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(a))) =>
-        t should be ("2000-01-02")
-        f should be (0.841470985)
-        a should be (0.16296098992018704)
-    }
-  }
+//  it should "work with Text times" in {
+//    val ds = Dataset.fromName("sine_wave_with_anomalies_text")
+//      .withOperation(ModelWithAutoencoder())
+//
+//    //TextWriter().write(ds)
+//
+//    StreamUtils.unsafeHead(ds.samples) match {
+//      case Sample(DomainData(Text(t)), RangeData(Real(f), Real(a))) =>
+//        t should be ("2000-01-02")
+//        f should be (0.841470985)
+//        a should be (0.16296098992018704)
+//    }
+//  }
 
   "The DetectAnomalies operation" should "work with a rolling mean model" in {
     val ds = Dataset.fromName("sine_wave_with_anomalies")
