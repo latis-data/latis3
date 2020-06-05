68947e435f0909758a1ab79724d123c5426a0a38
Ryan Heins <ryan.heins@lasp.colorado.edu>
2020-06-05 13:23:12 -0600

    Add operations to FDML

--- core/src/test/scala/latis/input/fdml/FdmlReaderSpec.scala
+++ core/src/test/scala/latis/input/fdml/FdmlReaderSpec.scala
@@ -6,6 +6,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
 import latis.data._
+import latis.model._
 import latis.util.StreamUtils
 
 class FdmlReaderSpec extends FlatSpec {
@@ -28,5 +29,20 @@ class FdmlReaderSpec extends FlatSpec {
     }
 
     err.getMessage should include ("'{source}' is expected")
+  }
+
+  it should "apply operations to the returned dataset" in {
+    val ds = FdmlReader.read(new URI("datasets/dataWithOperations.fdml"), true)
+
+    StreamUtils.unsafeHead(ds.samples) match {
+      case Sample(DomainData(Number(t)), RangeData(Integer(b), Real(c))) =>
+        assert(t == 1)
+        assert(b == 2)
+        assert(c == 2.2)
+    }
+    ds.model match {
+      case Function(_, range) =>
+        assert(range.getScalars.map(_.id) == List("b", "Istanbul"))
+    }
   }
 }
