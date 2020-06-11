2c17880738ebbe7965b4f58d4c17b525b6d5abb6
Shawn Polson <sapols@yahoo.com>
2020-06-11 17:39:00 -0600

    Add a comment about installing JEP separately

--- travis/python_build.sh
+++ travis/python_build.sh
@@ -7,6 +7,7 @@ then
 else
   pip -q install -U pip
   pip -q install -r python/requirements/requirements.txt
+  # install JEP separately to ensure it is built with numpy support 
   pip -q install -r python/requirements/jep.txt
   cp -r /opt/python/3.6/lib/python3.6/site-packages $HOME/local/lib
 fi
