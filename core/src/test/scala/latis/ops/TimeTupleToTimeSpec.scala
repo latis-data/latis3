c1d84ab2d3a4622c6ae7dea24168c555a782058c
Shawn Polson <sapols@yahoo.com>
2020-07-30 12:21:15 -0500

    Replace text data and test FDML with a mock dataset in code

--- core/src/test/scala/latis/ops/TimeTupleToTimeSpec.scala
+++ core/src/test/scala/latis/ops/TimeTupleToTimeSpec.scala
@@ -1,18 +1,41 @@
 package latis.ops
 
 import latis.data._
-import latis.dataset.Dataset
+import latis.dataset.MemoizedDataset
 import latis.metadata.Metadata
 import latis.model._
 import latis.output.TextWriter
 import latis.util.StreamUtils
+
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
 class TimeTupleToTimeSpec extends FlatSpec {
+  
+  def mockDataset: MemoizedDataset = {
+    val metadata: Metadata = Metadata("TestTimeTupleFlux" + ("title" -> "Test Time Tuple Flux"))
+    val model: DataType = Function(
+      Tuple(Metadata("time"),
+        Scalar(Metadata("year") + ("type" -> "int") + ("units" -> "yyyy")),
+        Scalar(Metadata("month") + ("type" -> "int") + ("units" -> "MM")),
+        Scalar(Metadata("day") + ("type" -> "int") + ("units" -> "dd")),
+      ),
+      Scalar(Metadata("flux") + ("type" -> "int"))
+    )
+    val data: MemoizedFunction = SeqFunction(
+      Seq(
+        Sample(DomainData(Data.IntValue(1945), Data.IntValue(1), Data.IntValue(1)), RangeData(10)),
+        Sample(DomainData(Data.IntValue(1945), Data.IntValue(1), Data.IntValue(2)), RangeData(20)),
+        Sample(DomainData(Data.IntValue(1945), Data.IntValue(1), Data.IntValue(3)), RangeData(30)),
+        Sample(DomainData(Data.IntValue(1945), Data.IntValue(1), Data.IntValue(4)), RangeData(40)),
+        Sample(DomainData(Data.IntValue(1945), Data.IntValue(1), Data.IntValue(5)), RangeData(50))
+      )
+    )
+    new MemoizedDataset(metadata, model, data)
+  }
 
-  "The Daily Flux dataset's time tuple" should "be converted to a time scalar" in {
-    val ds = Dataset.fromName("dailyFlux")
+  "The mock dataset's time tuple" should "be converted to a time scalar" in {
+    val ds = mockDataset
 
     StreamUtils.unsafeHead(ds.samples) match {
       case Sample(DomainData(Number(y), Number(m), Number(d)), RangeData(Number(f))) =>
