f7b3896a0f67b8e24747593e2f515264663ca811
Shawn Polson <sapols@yahoo.com>
2020-08-05 13:30:30 -0600

    Add commented-out code that finds position and length of Time Tuple

--- core/src/main/scala/latis/ops/TimeTupleToTime.scala
+++ core/src/main/scala/latis/ops/TimeTupleToTime.scala
@@ -19,10 +19,15 @@ import cats.implicits._
 case class TimeTupleToTime(name: String = "time") extends UnaryOperation {
   
   override def applyToModel(model: DataType): DataType = {
-    //val timePos: Int = model.getPath(name) match {
-    //  case Some(List(RangePosition(n))) => n
-    //  case _ => throw new LatisException(s"Cannot find variable: $name")
-    //}
+//    val timePos: Int = model.getPath(name) match {
+//      case Some(List(DomainPosition(n))) => n 
+//      //TODO: what if it's RangePosition instead?
+//      case _ => throw new LatisException(s"Cannot find path to variable: $name")
+//    }
+//    val timeLen: Int = model.findAllVariables(name).head match {
+//      case Tuple(es @ _*) => es.length
+//      case _ => throw new LatisException(s"Cannot find variable: $name")
+//    }
     
     val timeTuple = model.findAllVariables(name).head
     
@@ -45,6 +50,16 @@ case class TimeTupleToTime(name: String = "time") extends UnaryOperation {
 
   override def applyToData(data: SampledFunction, model: DataType): SampledFunction = {
     val samples = data.unsafeForce.sampleSeq //TODO: don't unsafeForce
+
+//    val timePos: Int = model.getPath(name) match {
+//      case Some(List(DomainPosition(n))) => n
+//      //TODO: what if it's RangePosition instead?
+//      case _ => throw new LatisException(s"Cannot find path to variable: $name")
+//    }
+//    val timeLen: Int = model.findAllVariables(name).head match {
+//      case Tuple(es @ _*) => es.length
+//      case _ => throw new LatisException(s"Cannot find variable: $name")
+//    }
 
     val convertedSamples = samples.map {
       case Sample(dd, rd) =>
