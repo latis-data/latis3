f9f09352de7168023cd3b3c04dfe1e5e504f8f3c
sapols <sapols@yahoo.com>
2020-09-03 11:08:55 -0600

    Add a couple test cases for timeTupleToTime and alphabetize

--- core/src/test/scala/latis/ops/UnaryOperationSpec.scala
+++ core/src/test/scala/latis/ops/UnaryOperationSpec.scala
@@ -8,11 +8,13 @@ import latis.ops.UnaryOperation._
 class UnaryOperationSpec extends FlatSpec {
 
   "makeOperation" should "make unary operations" in {
+    makeOperation("curry", List()) should be (Right(Curry()))
+    makeOperation("curry", List("2")) should be (Right(Curry(2)))
+    makeOperation("evaluation", List("1")) should be (Right(Evaluation("1")))
+    makeOperation("pivot", List("(1,2)", "(Fe,Mg)")) should be (Right(Pivot(Seq("1", "2"), Seq("Fe", "Mg"))))
     makeOperation("project", List("a", "b")) should be (Right(Projection("a", "b")))
     makeOperation("rename", List("a", "b")) should be (Right(Rename("a", "b")))
-    makeOperation("curry", List("2")) should be (Right(Curry(2)))
-    makeOperation("curry", List()) should be (Right(Curry()))
-    makeOperation("pivot", List("(1,2)", "(Fe,Mg)")) should be (Right(Pivot(Seq("1", "2"), Seq("Fe", "Mg"))))
-    makeOperation("evaluation", List("1")) should be (Right(Evaluation("1")))
+    makeOperation("timeTupleToTime", List()) should be (Right(TimeTupleToTime()))
+    makeOperation("timeTupleToTime", List("Time")) should be (Right(TimeTupleToTime("Time")))
   }
 }
