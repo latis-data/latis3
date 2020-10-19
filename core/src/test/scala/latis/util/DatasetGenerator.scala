12a729ae538794d37292784243a85f739fd17e4d
Ryan Heins <ryan.heins@lasp.colorado.edu>
2020-10-19 13:25:26 -0600

    Add DatasetGenerator apply and tests

--- core/src/test/scala/latis/util/DatasetGenerator.scala
+++ core/src/test/scala/latis/util/DatasetGenerator.scala
@@ -2,31 +2,120 @@ package latis.util
 
 import java.util.UUID
 
-import cats.implicits._
+import atto.Atto._
+import atto.ParseResult
+import atto.Parser
 
-import latis.data.CartesianFunction1D
-import latis.data.CartesianFunction2D
-import latis.data.CartesianFunction3D
-import latis.data.DomainData
-import latis.data.MemoizedFunction
-import latis.data.RangeData
-import latis.data.Sample
-import latis.data.SampledFunction
+import latis.data.Data._
+import latis.data._
 import latis.dataset.MemoizedDataset
 import latis.metadata.Metadata
-import latis.model.DataType
-import latis.model.Function
-import latis.model.Scalar
-import latis.model.Tuple
-import latis.model.ValueType
-import latis.util.DataUtils.anySeqToDatumSeq
+import latis.model._
 
 /**
  * Provides convenient methods for constructing Datasets for testing.
  */
 object DatasetGenerator {
   //TODO: add to DSL
-  //TODO: make scalacheck generators
+
+  /**
+   * Generates a dataset of a few samples with the model specified in the input
+   * string. The values, starting with the first sample, increment depending on
+   * their type. Domain variables increment independently.
+   * {{{
+   *   > DatasetGenerator("double -> (double, boolean)")
+   * }}}
+   * returns a dataset where the first two samples are
+   * 0.0 -> (0.0, true)
+   * 1.0 -> (1.0, false)
+   * If a multi-dimensional dataset is specified, then a cartesian dataset is
+   * returned where the first variable in the domain is of size 2, the second
+   * size 3, etc.
+   * {{{
+   *   > DatasetGenerator("(string, int) -> (double, double)")
+   * }}}
+   * returns a dataset where the samples are:
+   * ("a", 0) -> (0.0, 1.0)
+   * ("a", 1) -> (2.0, 3.0)
+   * ("a", 2) -> (4.0, 5.0)
+   * ("b", 0) -> (6.0, 7.0)
+   * ("b", 1) -> (8.0, 9.0)
+   * ("b", 2) -> (10.0, 11.0)
+   *
+   */
+  def apply(typeSig: String): MemoizedDataset = {
+    val model = modelFromString(typeSig)
+    val ds = new MemoizedDataset(
+      Metadata(makeDatasetID()),
+      model,
+      generateData(model)
+    )
+    ds
+  }
+
+  private def modelFromString(s: String): DataType = {
+    val absModel = modelParser.model.parseOnly(s) match {
+      case ParseResult.Done(_, m) => m
+      case _ => throw LatisException(s"failed to parse $s")
+    }
+    modelFromAst(absModel)
+  }
+
+  private def modelFromAst(absModel: modelAst.Model): DataType = absModel match {
+    case modelAst.Function(d, r) => Function(modelFromAst(d), modelFromAst(r))
+    case modelAst.Tuple(l) => Tuple(l.map(modelFromAst))
+    case modelAst.Scalar(t) => Scalar(Metadata(t.toString) + ("type" -> t.toString))
+  }
+
+  private def generateData(m: DataType): MemoizedFunction = m.arity match {
+    case 1 => generateData1D(m)
+    case 2 => generateData2D(m)
+    case 3 => generateData3D(m)
+    case a => throw LatisException(s"DatasetGenerator doesn't support datasets of arity $a")
+  }
+
+  private def generateData1D(m: DataType): MemoizedFunction = {
+    val scalarTypes = m.getScalars.map(_.valueType)
+    val dGen = DataGenerator(scalarTypes.head)
+    val rGen = new RangeGenerator(m)
+    val numSamples = 3
+    val samples = for {
+      d <- dGen.take(numSamples).toList
+    } yield {
+      Sample(DomainData(d), rGen.next())
+    }
+    SampledFunction(samples)
+  }
+
+  private def generateData2D(m: DataType): MemoizedFunction = {
+    val scalarTypes = m.getScalars.map(_.valueType)
+    val d1s = DataGenerator(scalarTypes.head).take(2).toList
+    val d2s = DataGenerator(scalarTypes(1)).take(3).toList
+    val rGen = new RangeGenerator(m)
+    val samples = for {
+      d1 <- d1s
+      d2 <- d2s
+    } yield {
+      Sample(DomainData(d1, d2), rGen.next())
+    }
+    SampledFunction(samples)
+  }
+
+  private def generateData3D(m: DataType): MemoizedFunction = {
+    val scalarTypes = m.getScalars.map(_.valueType)
+    val d1s = DataGenerator(scalarTypes.head).take(2).toList
+    val d2s = DataGenerator(scalarTypes(1)).take(3).toList
+    val d3s = DataGenerator(scalarTypes(2)).take(4).toList
+    val rGen = new RangeGenerator(m)
+    val samples = for {
+      d1 <- d1s
+      d2 <- d2s
+      d3 <- d3s
+    } yield {
+      Sample(DomainData(d1, d2, d3), rGen.next())
+    }
+    SampledFunction(samples)
+  }
 
   def generate1DDataset(
     xs: Seq[Any],
@@ -106,4 +195,125 @@ object DatasetGenerator {
       case _ => Tuple(scalars)
     }
   }
+}
+
+class BooleanGenerator extends Iterator[Datum] {
+  def hasNext: Boolean = true
+  private var lastValue = false
+  def next(): BooleanValue = {
+    lastValue = !lastValue
+    BooleanValue(lastValue)
+  }
+}
+
+class IntGenerator extends Iterator[Datum] {
+  def hasNext: Boolean = true
+  private var lastValue = -1
+  def next(): IntValue = {
+    lastValue += 1
+    IntValue(lastValue)
+  }
+}
+
+class DoubleGenerator extends Iterator[Datum] {
+  def hasNext: Boolean = true
+  private var lastValue = -1.0
+  def next(): DoubleValue = {
+    lastValue += 1.0
+    DoubleValue(lastValue)
+  }
+}
+
+class StringGenerator extends Iterator[Datum] {
+  def hasNext: Boolean = true
+  private var lastValue = "`"
+  def next(): StringValue = {
+    val nextAscii = if (lastValue == 'z') 97 else lastValue.head.toInt + 1
+    lastValue = nextAscii.toChar.toString
+    StringValue(lastValue)
+  }
+}
+
+object DataGenerator {
+  def apply(vType: ValueType): Iterator[Datum] = vType match {
+    case BooleanValueType => new BooleanGenerator
+    case IntValueType => new IntGenerator
+    case DoubleValueType => new DoubleGenerator
+    case StringValueType => new StringGenerator
+    case t => throw LatisException(s"No generator for $t")
+  }
+}
+
+class RangeGenerator(model: DataType) extends Iterator[RangeData] {
+  def hasNext: Boolean = true
+  private val boolGen = DataGenerator(BooleanValueType)
+  private val intGen = DataGenerator(IntValueType)
+  private val doubleGen = DataGenerator(DoubleValueType)
+  private val stringGen = DataGenerator(StringValueType)
+
+  private def nextOfType(t: ValueType): Datum = t match {
+    case BooleanValueType => boolGen.next()
+    case IntValueType => intGen.next()
+    case DoubleValueType => doubleGen.next()
+    case StringValueType => stringGen.next()
+    case t => throw LatisException(s"No generator for $t")
+  }
+  private val rangeTypes = model.getScalars.drop(model.arity).map(_.valueType)
+  def next(): RangeData = RangeData(rangeTypes.map(nextOfType))
+}
+
+object modelParser {
+  def model: Parser[modelAst.Model] =
+    parens(function) |function | tuple | scalar
+
+  /** Doesn't support nested functions in the domain. */
+  def function: Parser[modelAst.Model] = for {
+    d <- (tuple | scalar).token
+    _ <- string("->").token
+    r <- model.token
+  } yield modelAst.Function(d, r)
+
+  /** Only parses tuples of scalars */
+  def tuple: Parser[modelAst.Model] = for {
+    l <- parens(sepBy(scalar.token, char(',').token))
+  } yield modelAst.Tuple(l)
+
+  def scalar: Parser[modelAst.Model] = for {
+    t <- valueType
+  } yield modelAst.Scalar(t)
+
+  def valueType: Parser[ValueType] =
+    booleanType | intType | doubleType | stringType
+
+  def booleanType: Parser[ValueType] = for {
+    _ <- string("boolean") | string("Boolean")
+  } yield BooleanValueType
+
+  def intType: Parser[ValueType] = for {
+    _ <- string("int") | string("Int")
+  } yield IntValueType
+
+  def doubleType: Parser[ValueType] = for {
+    _ <- string("double") | string("Double")
+  } yield DoubleValueType
+
+  def stringType: Parser[ValueType] = for {
+    _ <- string("string") | string("String")
+  } yield StringValueType
+}
+
+object modelAst {
+  sealed trait Model
+
+  final case class Function(
+    domain: Model, range: Model
+  ) extends Model
+
+  final case class Tuple(
+    elements: List[Model]
+  ) extends Model
+
+  final case class Scalar(
+    valueType: ValueType
+  ) extends Model
 }
--- core/src/test/scala/latis/util/DatasetGeneratorSpec.scala
+++ core/src/test/scala/latis/util/DatasetGeneratorSpec.scala
@@ -1,5 +1,8 @@
 package latis.util
 
+import atto.Atto._
+import atto.ParseResult
+import atto.Parser
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
@@ -82,5 +85,103 @@ class DatasetGeneratorSpec extends FlatSpec {
     ds.data.sampleSeq.drop(1).head should be (Sample(DomainData('a', 1, 0.2), RangeData("a1.2", 2.2)))
     ds.data.sampleSeq.drop(2).head should be (Sample(DomainData('a', 2, 0.1), RangeData("a2.1", 3.3)))
     ds.data.sampleSeq.drop(4).head should be (Sample(DomainData('b', 1, 0.1), RangeData("b1.1", 5.5)))
+  }
+
+  "fromString" should "generate a 1D dataset" in {
+    val ds = DatasetGenerator("double -> (double, boolean)")
+    ds.data.sampleSeq(0) should be (Sample(DomainData(0.0), RangeData(0.0, true)))
+    ds.data.sampleSeq(1) should be (Sample(DomainData(1.0), RangeData(1.0, false)))
+    ds.data.sampleSeq(2) should be (Sample(DomainData(2.0), RangeData(2.0, true)))
+  }
+
+  it should "generate a 2D dataset" in {
+    val ds = DatasetGenerator("(string, int) -> (double, double)")
+    ds.data.sampleSeq(0) should be (Sample(DomainData("a", 0), RangeData(0.0, 1.0)))
+    ds.data.sampleSeq(1) should be (Sample(DomainData("a", 1), RangeData(2.0, 3.0)))
+    ds.data.sampleSeq(2) should be (Sample(DomainData("a", 2), RangeData(4.0, 5.0)))
+    ds.data.sampleSeq(3) should be (Sample(DomainData("b", 0), RangeData(6.0, 7.0)))
+    ds.data.sampleSeq(4) should be (Sample(DomainData("b", 1), RangeData(8.0, 9.0)))
+    ds.data.sampleSeq(5) should be (Sample(DomainData("b", 2), RangeData(10.0, 11.0)))
+  }
+
+  it should "generate a 3D dataset" in {
+    val ds = DatasetGenerator("(string, int, int) -> double")
+    ds.data.sampleSeq(0)  should be (Sample(DomainData("a", 0, 0), RangeData(0.0)))
+    ds.data.sampleSeq(1)  should be (Sample(DomainData("a", 0, 1), RangeData(1.0)))
+    ds.data.sampleSeq(4)  should be (Sample(DomainData("a", 1, 0), RangeData(4.0)))
+    ds.data.sampleSeq(11) should be (Sample(DomainData("a", 2, 3), RangeData(11.0)))
+    ds.data.sampleSeq(12) should be (Sample(DomainData("b", 0, 0), RangeData(12.0)))
+    ds.data.sampleSeq(23) should be (Sample(DomainData("b", 2, 3), RangeData(23.0)))
+  }
+}
+
+class ModelParserSpec extends FlatSpec {
+
+  "modelParser" should "parse scalars" in {
+    val testScalar = testParser(modelParser.scalar)(_, _)
+    testScalar("double", modelAst.Scalar(DoubleValueType))
+    testScalar("int", modelAst.Scalar(IntValueType))
+  }
+
+  "modelParser" should "parse tuples" in {
+    val testTuple = testParser(modelParser.tuple)(_, _)
+    testTuple("(double, int)",
+      modelAst.Tuple(List(
+        modelAst.Scalar(DoubleValueType),
+        modelAst.Scalar(IntValueType)
+      ))
+    )
+  }
+
+  "modelParser" should "parse functions" in {
+    val testFunction = testParser(modelParser.function)(_, _)
+    testFunction("int -> double",
+      modelAst.Function(
+        modelAst.Scalar(IntValueType),
+        modelAst.Scalar(DoubleValueType)
+      )
+    )
+    testFunction("(string, int) -> (double, double)",
+      modelAst.Function(
+        modelAst.Tuple(List(
+          modelAst.Scalar(StringValueType),
+          modelAst.Scalar(IntValueType)
+        )),
+        modelAst.Tuple(List(
+          modelAst.Scalar(DoubleValueType),
+          modelAst.Scalar(DoubleValueType)
+        ))
+      )
+    )
+    testFunction("int -> double -> double",
+      modelAst.Function(
+        modelAst.Scalar(IntValueType),
+        modelAst.Function(
+          modelAst.Scalar(DoubleValueType),
+          modelAst.Scalar(DoubleValueType)
+        )
+      )
+    )
+    testFunction("int -> (double -> double)",
+      modelAst.Function(
+        modelAst.Scalar(IntValueType),
+        modelAst.Function(
+          modelAst.Scalar(DoubleValueType),
+          modelAst.Scalar(DoubleValueType)
+        )
+      )
+    )
+  }
+
+  /**
+   * Partially apply with a parser to get a function that takes the string you
+   * want to parse and the thing you expect to get back
+   */
+  private def testParser[A](p: Parser[A])(s: String, d: A): Unit = p.parseOnly(s) match {
+    case ParseResult.Done(_, result) => result should be (d)
+    case ParseResult.Fail(_, _, m) => fail(s"$m in $s")
+    // parseOnly will never return anything but Done or Fail, but the types don't
+    // know that so we get a warning without the following line.
+    case _ => fail(s"failed to parse $s")
   }
 }
