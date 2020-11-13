a9f1f8e0c2cca2f46590607c044e8a7a3c679c6b
Ryan Heins <ryan.heins@lasp.colorado.edu>
2020-11-13 14:52:48 -0700

    Add core tests as dependency of netcdf tests

--- netcdf/src/test/scala/latis/input/NetcdfWrapperSpec.scala
+++ netcdf/src/test/scala/latis/input/NetcdfWrapperSpec.scala
@@ -7,43 +7,14 @@ import org.scalatest.Matchers._
 import ucar.ma2.Section
 import ucar.nc2.dataset.NetcdfDataset
 
-import latis.data.DomainData
-import latis.data.RangeData
-import latis.data.Sample
-import latis.data.SampledFunction
 import latis.dataset.MemoizedDataset
-import latis.metadata.Metadata
-import latis.model._
 import latis.output.NetcdfEncoder
-import latis.util.Identifier.IdentifierStringContext
+import latis.util.DatasetGenerator
 
 class NetcdfWrapperSpec extends FlatSpec {
 
-  private val mock2d: MemoizedDataset = {
-    val samples = Seq(
-      Sample(DomainData(1, "1"), RangeData(1.1, 0.1)),
-      Sample(DomainData(1, "2"), RangeData(1.2, 0.2)),
-      Sample(DomainData(2, "1"), RangeData(2.1, 0.3)),
-      Sample(DomainData(2, "2"), RangeData(2.2, 0.4)),
-      Sample(DomainData(3, "1"), RangeData(3.1, 0.5)),
-      Sample(DomainData(3, "2"), RangeData(3.2, 0.6))
-    )
-
-    val md = Metadata(id"mock2d")
-    val model = Function(
-      Tuple(
-        Scalar(Metadata(id"_1") + ("type" -> "int")),
-        Scalar(Metadata(id"_2") + ("type" -> "string"))
-      ),
-      Tuple(
-        Scalar(Metadata(id"a") + ("type" -> "double")),
-        Scalar(Metadata(id"b") + ("type" -> "double"))
-      )
-    )
-    val data = SampledFunction(samples)
-
-    new MemoizedDataset(md, model, data)
-  }
+  private val mock2d: MemoizedDataset =
+    DatasetGenerator("(a, b) -> (c, d)")
 
   private def makeSections(s: String): List[Section] =
     s.split(';').toList.map(new Section(_))
@@ -57,18 +28,18 @@ class NetcdfWrapperSpec extends FlatSpec {
     val enc = NetcdfEncoder(File.createTempFile("netcdf_test", ".nc", null))
     val file = enc.encode(mock2d).compile.toList.unsafeRunSync().head
     val netcdfDataset = NetcdfDataset.openDataset(file.toString)
-    val config2d = makeConfig("0:2; 0:1; 0:2, 0:1; 0:2, 0:1")
+    val config2d = makeConfig("0:1; 0:2; 0:1, 0:2; 0:1, 0:2")
     val wrapper = NetcdfWrapper(netcdfDataset, mock2d.model, config2d)
-    wrapper.sections should be(makeSections("0:2; 0:1; 0:2, 0:1; 0:2, 0:1"))
+    wrapper.sections should be(makeSections("0:1; 0:2; 0:1, 0:2; 0:1, 0:2"))
   }
 
   it should "replace null ranges in the adapter config string" in {
     val enc = NetcdfEncoder(File.createTempFile("netcdf_test", ".nc", null))
     val file = enc.encode(mock2d).compile.toList.unsafeRunSync().head
     val netcdfDataset = NetcdfDataset.openDataset(file.toString)
-    val config2d = makeConfig(":; 0:1; :, 0:1; :, 0:1")
+    val config2d = makeConfig(":; :; :, :; :, :")
     val wrapper = NetcdfWrapper(netcdfDataset, mock2d.model, config2d)
-    wrapper.sections should be(makeSections("0:2; 0:1; 0:2, 0:1; 0:2, 0:1"))
+    wrapper.sections should be(makeSections("0:1; 0:2; 0:1, 0:2; 0:1, 0:2"))
   }
 
 }
