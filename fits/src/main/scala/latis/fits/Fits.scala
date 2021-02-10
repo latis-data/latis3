48066a0ae526d20bd04b4c4edc7f7869a60473ff
Ryan Heins <ryan.heins@lasp.colorado.edu>
2021-02-10 15:49:31 -0700

    Use NonEmptyList

--- fits/src/main/scala/latis/fits/Fits.scala
+++ fits/src/main/scala/latis/fits/Fits.scala
@@ -1,17 +1,5 @@
 package latis.fits
 
-import cats.syntax.all._
+import cats.data.NonEmptyList
 
-import latis.util.LatisException
-
-case class Fits private (hdus: List[Hdu]) {
-
-}
-
-object Fits {
-  def fromHdus(hdus: List[Hdu]): Either[LatisException, Fits] = hdus match {
-    case l@(_:ImageHdu :: _) => Fits(l).asRight
-    case _ => Left(LatisException("A FITS file must be a non-empty list of" +
-      " HDUs, starting with an image HDU."))
-  }
-}
+case class Fits private (hdus: NonEmptyList[Hdu])
