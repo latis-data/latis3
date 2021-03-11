e970272a4316eb6f32035ab9016545fd56391338
Shawn Polson <sapols@yahoo.com>
2021-03-11 14:31:40 -0700

    Add content types
    TODO: verify safety of jsonl media type

--- dap2-service/src/main/scala/latis/service/dap2/Dap2Service.scala
+++ dap2-service/src/main/scala/latis/service/dap2/Dap2Service.scala
@@ -10,7 +10,9 @@ import cats.syntax.all._
 import fs2.Stream
 import fs2.io
 import fs2.text
+import org.http4s.headers.`Content-Type`
 import org.http4s.HttpRoutes
+import org.http4s.MediaType
 import org.http4s.Response
 import org.http4s.dsl.Http4sDsl
 
@@ -41,7 +43,7 @@ class Dap2Service extends ServiceInterface with Http4sDsl[IO] {
           ops      <- IO.fromEither(getOperations(req.queryString))
           result    = ops.foldLeft(dataset)((ds, op) => ds.withOperation(op))
           bytes    <- IO.fromEither(encode(ext, result))
-          response <- Ok(bytes)
+          response <- Ok(bytes._1).map(_.withContentType(bytes._2))
         } yield response).handleErrorWith {
           case err: Dap2Error => handleDap2Error(err)
           case _              => InternalServerError()
@@ -81,13 +83,15 @@ class Dap2Service extends ServiceInterface with Http4sDsl[IO] {
   private def stripQuotes(str: String): String =
     str.stripPrefix("\"").stripSuffix("\"")
 
-  private def encode(ext: String, ds: Dataset): Either[Dap2Error, Stream[IO, Byte]] = ext match {
+  private def encode(ext: String, ds: Dataset): Either[Dap2Error, (Stream[IO, Byte], `Content-Type`)] = ext match {
     case ""     => encode("html", ds)
     case "bin"  => new BinaryEncoder().encode(ds).flatMap {
       bits => Stream.emits(bits.toByteArray)
-    }.asRight
+    }.asRight.map((_, `Content-Type`(MediaType.application.`octet-stream`)))
     case "csv"  => CsvEncoder.withColumnName.encode(ds).through(text.utf8Encode).asRight
+      .map((_, `Content-Type`(MediaType.text.csv)))
     case "jsonl" => new JsonEncoder().encode(ds).map(_.noSpaces).intersperse("\n").through(text.utf8Encode).asRight
+      .map((_, `Content-Type`(MediaType.unsafeParse("application/jsonl")))) //TODO: verify safety of unsafeParse
     case "nc"   =>
       implicit val cs = StreamUtils.contextShift
       (for {
@@ -97,9 +101,11 @@ class Dap2Service extends ServiceInterface with Http4sDsl[IO] {
         )
         file    <- new NetcdfEncoder(tmpFile.toFile()).encode(ds)
         bytes   <- io.file.readAll[IO](file.toPath(), StreamUtils.blocker, 4096)
-      } yield bytes).asRight
+      } yield bytes).asRight.map((_, `Content-Type`(MediaType.application.`x-netcdf`)))
     case "txt"  => new TextEncoder().encode(ds).through(text.utf8Encode).asRight
+      .map((_,`Content-Type`(MediaType.text.plain)))
     case "meta" => new MetadataEncoder().encode(ds).map(_.noSpaces).through(text.utf8Encode).asRight
+      .map((_,`Content-Type`(MediaType.application.json)))
     case _      => UnknownExtension(s"Unknown extension: $ext").asLeft
   }
 
