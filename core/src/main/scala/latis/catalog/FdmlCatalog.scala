70d94ef4fec9769555e4aae07f46387f7d9bd644
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2021-03-11 11:22:32 -0700

    Add FDML catalog from classpath

--- core/src/main/scala/latis/catalog/FdmlCatalog.scala
+++ core/src/main/scala/latis/catalog/FdmlCatalog.scala
@@ -1,6 +1,8 @@
 package latis.catalog
 
+import java.net.URL
 import java.nio.file.Path
+import java.nio.file.Paths
 
 import cats.effect.IO
 import cats.syntax.all._
@@ -22,12 +24,41 @@ object FdmlCatalog {
    * and does not recurse into subdirectories.
    */
   def fromDirectory(path: Path, validate: Boolean = true): Catalog = new Catalog {
-    override val datasets: Stream[IO, Dataset] =
-      directoryStream[IO](StreamUtils.blocker, path, "*.fdml").flatMap { f =>
-        // TODO: Log failures to read datasets.
-        pathToDataset(f, validate).fold(_ => Stream.empty, Stream.emit)
-      }
+    override val datasets: Stream[IO, Dataset] = dirDatasetStream(path, validate)
   }
+
+  /**
+   * Creates a catalog from a directory of FDML files on the
+   * classpath.
+   *
+   * This catalog will only include files with the "fdml" extension
+   * and does not recurse into subdirectories.
+   */
+  def fromClasspath(
+    cl: ClassLoader,
+    path: Path,
+    validate: Boolean = true
+  ): Catalog = new Catalog {
+    override val datasets: Stream[IO, Dataset] = for {
+      url <- Stream.eval(getResource(cl, path.toString())).unNone
+      dir <- Stream.fromEither[IO](urlToPath(url))
+      ds  <- dirDatasetStream(dir, validate)
+    } yield ds
+
+    private def getResource(
+      cl: ClassLoader,
+      resource: String
+    ): IO[Option[URL]] = IO(Option(cl.getResource(resource)))
+
+    private def urlToPath(url: URL): Either[Throwable, Path] =
+      Either.catchNonFatal(Paths.get(url.toURI()))
+  }
+
+  private def dirDatasetStream(dir: Path, validate: Boolean): Stream[IO, Dataset] =
+    directoryStream[IO](StreamUtils.blocker, dir, "*.fdml").flatMap { f =>
+      // TODO: Log failures to read datasets.
+      pathToDataset(f, validate).fold(_ => Stream.empty, Stream.emit)
+    }
 
   private def pathToDataset(path: Path, validate: Boolean): Either[Throwable, Dataset] =
     Either.catchNonFatal {
