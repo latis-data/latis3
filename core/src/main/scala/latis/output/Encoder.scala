8d531428d11caef10065330e43131cfd1c0dc4c0
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2021-04-24 21:26:08 -0600

    Give Encoder a Functor instance

--- core/src/main/scala/latis/output/Encoder.scala
+++ core/src/main/scala/latis/output/Encoder.scala
@@ -1,5 +1,6 @@
 package latis.output
 
+import cats.Functor
 import fs2.Stream
 
 import latis.dataset.Dataset
@@ -8,4 +9,16 @@ trait Encoder[F[_], O] {
 
   /** Convert a `Dataset` into a `Stream` of `O`. */
   def encode(dataset: Dataset): Stream[F, O]
+}
+
+object Encoder {
+
+  implicit def encoderFunctor[F[_]]: Functor[Encoder[F, *]] =
+    new Functor[Encoder[F, *]] {
+      override def map[A, B](fa: Encoder[F, A])(f: A => B): Encoder[F, B] =
+        new Encoder[F, B] {
+          override def encode(dataset: Dataset): Stream[F, B] =
+            fa.encode(dataset).map(f)
+        }
+    }
 }
