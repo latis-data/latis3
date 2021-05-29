2c4664e9df650f9d576864d3185685cbc77421c6
Doug Lindholm <dlindhol@gmail.com>
2021-05-29 07:22:15 -0600

    Front load adapter applied operations
    This removes a hack that allowed adapters to handle any operations
    risking order of operation  problems.

--- core/src/main/scala/latis/dataset/AdaptedDataset.scala
+++ core/src/main/scala/latis/dataset/AdaptedDataset.scala
@@ -51,13 +51,8 @@ class AdaptedDataset(
     // Separate leading operation that the adapter can handle
     // from the rest. Note that we must preserve the order for safety.
     //TODO: "compile" the Operations to optimize the order of application
-    //val adapterOps = operations.takeWhile(adapter.canHandleOperation(_))
-    //val otherOps   = operations.drop(adapterOps.length)
-
-    // Hack to allow smart adapter to handle any operation without concern
-    // about order. Otherwise, simple processing instructions could prevent
-    // other optimizations.
-    val (adapterOps, otherOps) = operations.partition(adapter.canHandleOperation(_))
+    val adapterOps = operations.takeWhile(adapter.canHandleOperation(_))
+    val otherOps   = operations.drop(adapterOps.length)
 
     //TODO: add prov for adapter handled ops
 
