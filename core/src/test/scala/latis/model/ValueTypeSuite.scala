fdd1a765a7b1558e3406567e8a2392f22da96924
Doug Lindholm <dlindhol@gmail.com>
2021-05-29 12:53:15 -0600

    Add a test for ValueType equality

---
+++ core/src/test/scala/latis/model/ValueTypeSuite.scala
@@ -0,0 +1,12 @@
+package latis.model
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ValueTypeSuite extends AnyFunSuite {
+
+  test("value type equality") {
+    val svt = ValueType.fromName("string").toTry.get
+    assert(svt == StringValueType)
+    assert(svt != IntValueType)
+  }
+}
