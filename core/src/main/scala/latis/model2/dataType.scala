dd22d2d59b22dc1b02f9a40d2f0264d9fbd96dc9
Doug Lindholm <dlindhol@gmail.com>
2021-07-02 16:24:23 -0600

    fix getPath

--- core/src/main/scala/latis/model2/dataType.scala
+++ core/src/main/scala/latis/model2/dataType.scala
@@ -10,39 +10,41 @@ import latis.data.RangePosition
 import latis.data.SamplePath
 import latis.data.SamplePosition
 import latis.metadata.Metadata
-import latis.model.Index
+import latis.model.LongValueType
 import latis.model.ValueType
 import latis.util.Identifier
 import latis.util.LatisException
 
 sealed trait DataType {
+  //TODO: enforce uniqueness
+  //TODO: support qualified identifiers
+  //TODO: no Index in stand-alone (constant) Scalar or Tuple?
+
   //TODO: DataTypeOps?
-  //def id: Option[Identifier]?
+  //def id: Option[Identifier]? NO
   //  only Projection, Rename look for id for any type, will need attention with qid
   //def scalars: List[Scalar] = ??? //needed? do we just need qualified ids for each scalar?
   //  generally used to zip with sample values
   //  nonIndexScalars?
   //  often a crutch without consideration for Functions in Tuples
-  //  TODO: use this as an opportunity to sanity check assumptions about tuples and functions in tuples?
-  //def flatten: DataType?
+  //  TODO: use this as an opportunity to sanity check assumptions about tuples and functions in tuples
+  //def flatten: DataType? NO
   //  used on Tuple only: GroupByVariable, TimeTupleToTime
   //    but nested tuples not usually considered
-  //  and DataType.getPath
-  //  what about nested tuples in nested functions? probably not
-  //  note that we ofter use getScalars to avoid nesting issues
+  //  we often use getScalars to avoid nesting issues, not safe for Functions in Tuples
   //  use Tuple.flatElements
   //def rename or withId
   //  Pivot, Rename, DataType.flatten
   //  Note need copy constructor that subclasses can override so we preserve subclass
   //  TODO: need to enforce uniqueness but don't have scope
-  //    smells like job for operation
+  //    smells like job for operation but need to be able to preserve scalar subclass
   //    do we need to reconstruct from metadata with class?
-  //    private to operation?
   //arity? vs dimensionality? (#56)
   //  DatasetGenerator, Curry, Evaluation, Resample
-  //  note getScalars makes arity useless for nested tuples
+  //  note getScalars makes arity useless for nested tuples, though we do so buggily
+  //  TODO: use arity as an opportunity to root out bugs
   /*
-  map?
+  TODO: map?
     ConvertTime.scala:56
       Time => Time
     FormatTime.scala:57
@@ -54,9 +56,8 @@ sealed trait DataType {
     not generally safe for any type mapping, changes structure that we are traversing over
     limit to Scalar => Scalar? TTTT is on its own?
     substitution use case? uri => Function
-    special caes don't need general solution here, but might be handy abstraction that opens doors?
+    special cases don't need general solution here, but might be handy abstraction that opens doors?
    */
-  //fold?
 
   /** Makes fill data for this data type. */
   def fillData: Data = {
@@ -69,32 +70,7 @@ sealed trait DataType {
     Data.fromSeq(go(this, Seq.empty))
   }
 
-  def fold[A](
-    fs: Scalar   => A,
-    ft: Tuple    => A,
-    ff: Function => A
-  ): A = this match {
-    case s: Scalar   => fs(s)
-    case t: Tuple    => ft(t)
-    case f: Function => ff(f)
-  }
-
-  def findVariable2(id: Identifier): Option[DataType] = {
-    def fun(dt: DataType): Option[DataType] = dt.fold(
-      (s: Scalar) =>
-        Option.when(s.id == id)(s),
-      (t: Tuple)  =>
-        if (t.id.contains(id)) t.some
-        else t.elements.map(fun).collectFirst {case Some(dt) => dt},
-      (f: Function) =>
-        if (f.id.contains(id)) f.some
-        else fun(f.domain).orElse(fun(f.range))
-    )
-    fun(this)
-  }
-
   /** Finds the first variable with the given Identifier. */
-  //TODO: deal with QID
   def findVariable(id: Identifier): Option[DataType] = this match {
     case s: Scalar =>
       Option.when(s.id == id)(s)
@@ -107,35 +83,44 @@ sealed trait DataType {
   }
 
   /** Gets the path to the given variable in a Sample. */
-  //TODO: deal with QID
   def getPath(id: Identifier): Option[SamplePath] = {
-    // Helper function to make SamplePosition
-    def position(domain: Boolean, index: Int): SamplePosition =
-      if (domain) DomainPosition(index)
-      else RangePosition(index)
+    // Helper function to make SamplePosition.
+    // tdr: "t"op, "d"omain, or "r"ange
+    def position(tdr: String, index: Int): SamplePosition = tdr match {
+      case "t" => RangePosition(index)
+      case "d" => DomainPosition(index)
+      case "r" => RangePosition(index)
+      //TODO: nowarn annotation
+    }
 
-//    def go(dt: DataType, path: SamplePath, domain: Boolean, index: Int): Option[SamplePath] = dt match {
-//      case s: Scalar =>
-//        //TODO: exclude Index
-//        Option.when(s.id == id)(position(domain, index))
-////        if (s.id == id) path :+ position(domain, index) //TODO: not our job to add to list?
-////        else path
-//
-//      case t: Tuple => t.flatElements.filterNot(_.isInstanceOf[Index]).zipWithIndex.map {
-//        case (dt, i) => go(dt, path, domain, i)
-//        ???
-//      }
-//
-//      case Function(d, r) => ???
-//
-//    }
-//
-//    // Note: domain=false and index=0 is the SamplePosition for a constant.
-//    go(this, List.empty, false, 0) //TODO: Some or None if empty
-    /*
-    TODO: consider fold: https://files.slack.com/files-pri/T3JCZ2S87-F026Z303J8G/fun_with_qualified_identifiers.scala
-     */
-    ???
+    // Build list of id, path pairs
+    def accumulateIdPathPairs(
+      dt: DataType,
+      path: SamplePath,
+      tdr: String, // top, domain, or range
+      index: Int
+    ): List[(Identifier, SamplePath)] = dt match {
+      case _: Index  => List.empty //Index not represented in Sample
+      case s: Scalar => List((s.id, path :+ position(tdr, index)))
+      case t: Tuple  =>
+        // TupleData is not allowed in a Sample, so no path for a Tuple.
+        // Top level Tuple shows up in Sample range.
+        val newTdr = if (tdr == "t") "r" else tdr
+        // Samples do not preserve tuple nesting.
+        t.flatElements.zipWithIndex.flatMap { case (e, i) =>
+          accumulateIdPathPairs(e, path, newTdr, i)
+        }
+      case f: Function =>
+        // Top level function has no position in a Sample.
+        val newPath = if (tdr == "t") List.empty else path :+ position(tdr, index)
+        f.id.map((_, newPath)).toList ++ //empty if id not defined
+          accumulateIdPathPairs(f.domain, newPath, "d", 0) ++
+          accumulateIdPathPairs(f.range,  newPath, "r", 0)
+    }
+
+    // Find the path for the given id
+    //TODO: short circuit
+    accumulateIdPathPairs(this, List.empty, "t", 0).find(_._1 == id).map(_._2)
   }
 }
 
@@ -154,7 +139,7 @@ object DataType {
 
 //---- Scalar ----//
 
-class Scalar private (
+class Scalar private[model2] (
          val id: Identifier,
          val valueType: ValueType,
          //val units: Option[String] = None, //TODO: MeasurementScale? still need to support string for now
@@ -196,6 +181,12 @@ object Scalar {
     otherProperties = other
   ) //TODO: other properties
 
+}
+
+class Index(id: Identifier) extends Scalar(id, LongValueType)
+
+object Index {
+  def apply(id: Identifier): Index = new Index(id)
 }
 
 //---- Tuple ----//
@@ -245,6 +236,11 @@ object Tuple {
 class Function private (val id: Option[Identifier], val domain: DataType, val range: DataType) extends DataType {
 
   override def toString: String = id.map(_.asString +": ").getOrElse("") + s"$domain -> $range"
+  /*
+  TODO: parens: f: (x -> a)?
+    consider long form with scalar types: x: int -> a: int
+      only know x is not a function because "int" is reserved?
+   */
 }
 
 object Function {
@@ -252,6 +248,7 @@ object Function {
   def from(id: Option[Identifier], domain: DataType, range: DataType): Either[LatisException, Function] = {
     //TODO: assert unique ids
     //TODO: assert no functions in domain, including within tuples
+    //TODO: no Index in range
     new Function(id, domain, range).asRight
   }
 
--- core/src/test/scala/latis/model2/DataTypeSuite.scala
+++ core/src/test/scala/latis/model2/DataTypeSuite.scala
@@ -1,7 +1,12 @@
 package latis.model2
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.Inside.inside
 
+import latis.data.DomainPosition
+import latis.data.NullData
+import latis.data.RangePosition
+import latis.data.TupleData
 import latis.model.DoubleValueType
 import latis.model.IntValueType
 import latis.model.StringValueType
@@ -9,17 +14,94 @@ import latis.util.Identifier.IdentifierStringContext
 
 class DataTypeSuite extends AnyFunSuite {
 
+  //TODO: property based test: generate model with "a" at random path
+  //  make sure we can find it
+  //  make sure we can get matching path
+  //  https://www.scalatest.org/user_guide/generator_driven_property_checks
+
+  val i = Index(id"_i")
+  val x = Scalar(id"x", IntValueType)
+  val y = Scalar(id"y", IntValueType)
+  val z = Scalar(id"z", IntValueType)
   val a = Scalar(id"a", IntValueType)
   val b = Scalar(id"b", DoubleValueType)
   val c = Scalar(id"c", StringValueType)
-  val t = Tuple.fromElements(id"t", b, c).toTry.get
-  val f = Function.from(id"f", a, t).toTry.get
+  val namedTup  = Tuple.fromElements(id"t", a, b).toTry.get       // t: (a, b)
+  val anonTup   = Tuple.fromElements(a, b).toTry.get              // (a, b)
+  val nestedTup = Tuple.fromElements(namedTup, c).toTry.get       // (t: (a, b), c)
+  val f = Function.from(id"f", x, a).toTry.get                    // f: x -> a
+  val fWithTup = Function.from(id"f", x, namedTup).toTry.get      // f: x -> t: (a, b)
+  val tupWithF = Tuple.fromElements(a, fWithTup).toTry.get        // (a, f: x -> t: (a, b))
+  val nestedF = Function.from(id"g", z, f).toTry.get              // g: z -> f: x -> a
+  val nestedFInTup = Function.from(id"g", i, tupWithF).toTry.get  // g: _i -> (a, f: x -> t: (a, b))
 
   test("to string") {
-    assert(f.toString == "f: a -> t: (b, c)")
+    assert(nestedFInTup.toString == "g: _i -> (a, f: x -> t: (a, b))")
   }
 
   test("find tuple") {
-    assert(f.findVariable2(id"t").nonEmpty)
+    assert(nestedFInTup.findVariable(id"t").nonEmpty)
+  }
+
+  test("don't find") {
+    assert(f.findVariable(id"nope").isEmpty)
+  }
+
+  //---- getPath ----//
+
+  test("path to constant scalar") {
+    assert(a.getPath(id"a").contains(List(RangePosition(0))))
+  }
+
+  test("path to scalar in constant tuple") {
+    assert(namedTup.getPath(id"b").contains(List(RangePosition(1))))
+  }
+
+  test("path to scalar in nested tuple") {
+    assert(nestedTup.getPath(id"c").contains(List(RangePosition(2))))
+  }
+
+  test("path to function in top level tuple") {
+    assert(tupWithF.getPath(id"f").contains(List(RangePosition(1))))
+  }
+
+  test("no path to index") {
+    assert(nestedFInTup.findVariable(id"_i").nonEmpty)
+    assert(nestedFInTup.getPath(id"_i").isEmpty)
+  }
+
+  test("no path to tuple") {
+    assert(fWithTup.findVariable(id"t").nonEmpty)
+    assert(fWithTup.getPath(id"t").isEmpty)
+  }
+
+  test("path to scalar in function domain") {
+    assert(fWithTup.getPath(id"x").contains(List(DomainPosition(0))))
+  }
+
+  test("path to scalar in function range") {
+    assert(fWithTup.getPath(id"b").contains(List(RangePosition(1))))
+  }
+
+  test("path to scalar in nested function") {
+    assert(nestedF.getPath(id"a").contains(List(RangePosition(0),RangePosition(0))))
+  }
+
+  test("path to scalar in nested function in tuple") {
+    assert(nestedFInTup.getPath(id"b").contains(List(RangePosition(1),RangePosition(1))))
+  }
+
+  //---- fillData ----//
+
+  test("scalar fill data") {
+    assert(a.fillData == NullData)
+  }
+
+  test("tuple with function fill data") {
+    inside(tupWithF.fillData ) {
+      case TupleData(a, f) =>
+        assert(a == NullData)
+        assert(f == NullData)
+    }
   }
 }
