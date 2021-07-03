290d029e450cb8e6b29b07ccdd8bd75a7c31d7ff
Doug Lindholm <dlindhol@gmail.com>
2021-07-02 23:33:32 -0600

    Address compiler warnings

--- service-interface/src/main/scala/latis/server/ServiceInterface.scala
+++ service-interface/src/main/scala/latis/server/ServiceInterface.scala
@@ -1,11 +1,13 @@
 package latis.server
 
+import scala.annotation.unused
+
 import cats.effect.IO
 import org.http4s.HttpRoutes
 
 import latis.catalog.Catalog
 
-abstract class ServiceInterface(catalog: Catalog) {
+abstract class ServiceInterface(@unused catalog: Catalog) {
 
   def routes: HttpRoutes[IO]
 }
