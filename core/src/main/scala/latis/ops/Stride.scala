398957dfe6d3e54b93cfb281d589a40603113dc2
Doug Lindholm <dlindhol@gmail.com>
2021-09-02 14:03:27 -0600

    Specify that a Filter is idempotent, Stride is not

--- core/src/main/scala/latis/ops/Stride.scala
+++ core/src/main/scala/latis/ops/Stride.scala
@@ -1,17 +1,24 @@
 package latis.ops
 
+import cats.effect.IO
+import fs2._
+
 import latis.data.Sample
 import latis.model.DataType
+import latis.util.LatisException
 
 /**
  * Defines a unary operation that keeps every nth Sample
  * where n is the stride. The stride can have more than one element,
  * one for each dimension of a Cartesian Dataset.
  */
-case class Stride(stride: Seq[Int]) extends Filter {
+case class Stride(stride: Seq[Int]) extends StreamOperation {
   //TODO: support nD stride for Cartesian Datasets
 
-  def predicate(model: DataType): Sample => Boolean = {
+  def pipe(model: DataType): Pipe[IO, Sample, Sample] =
+    (stream: Stream[IO, Sample]) => stream.filter(predicate)
+
+  private val predicate: Sample => Boolean = {
     var count = -1
     (_: Sample) => {
       count = count + 1
@@ -19,6 +26,9 @@ case class Stride(stride: Seq[Int]) extends Filter {
       else false
     }
   }
+
+  // A Stride does not affect the model.
+  def applyToModel(model: DataType): Either[LatisException, DataType] = Right(model)
 
 }
 
