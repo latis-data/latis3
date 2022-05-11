a626466fc94bc70f5059f5c2720ffa3f9ed8fa0c
Jake Martin <56704419+JakeMartin99@users.noreply.github.com>
2022-05-11 09:26:29 -0600

    Support HTML view of nested catalogs

--- dap2-service/src/main/scala/latis/service/dap2/HtmlCatalogEncoder.scala
+++ dap2-service/src/main/scala/latis/service/dap2/HtmlCatalogEncoder.scala
@@ -1,42 +1,104 @@
 package latis.service.dap2
 
 import cats.effect._
+import fs2.Stream
 import scalatags.Text
 import scalatags.Text.all._
+import scalatags.Text.tags2.details
+import scalatags.Text.tags2.style
+import scalatags.Text.tags2.summary
 
 import latis.catalog.Catalog
 
 object HtmlCatalogEncoder {
 
-  /** Provides an HTML representation of a Catalog. */
-  def encode(catalog: Catalog): IO[Text.TypedTag[String]] =
-    catalogTable(catalog).map { table =>
+  private val emptyTab = raw("&nbsp&nbsp&nbsp&nbsp")
+  private val backArrow = raw("&#8689")
+  private val forwardArrow = raw("&#8690")
+
+  /** Provides a complete HTML representation of a Catalog. */
+  def encode(catalog: Catalog): IO[Text.TypedTag[String]] = {
+    for {
+      dsTable <- datasetTable(catalog)
+      scTable <- subcatalogTable(catalog)
+    } yield {
       html(
+        head(
+          style(
+            "table { height: 50px; padding: 15px 0; margin: 15px 0; border:2px solid; }" +
+              "th, td { padding: 0 15px; text-align: left; }" +
+              "caption { text-align: left; }" +
+              "summary { width: 100%; }" +
+              ".subcatalog { width: 100%; }"
+          )
+        ),
         body(
           h1("LaTiS 3 DAP2 Server"),
           hr(),
-          table
+          h2(b(i(u("Catalog"))), emptyTab, a(href := "../")(backArrow)),
+          div(Text.all.style := "max-width: 50%;",
+            dsTable,
+            scTable
+          ),
+          hr()
         )
       )
     }
+  }
 
-  /** Provides an HTML table representation of a Catalog. */
-  private[dap2] def catalogTable(catalog: Catalog): IO[Text.TypedTag[String]] =
+  /** Provides an HTML table representation of a Catalog's datasets */
+  private[dap2] def datasetTable(catalog: Catalog, prefix: String = ""): IO[Text.TypedTag[String]] =
     catalog.datasets.map { ds =>
       val id = ds.id.fold("")(_.asString)
       val title = ds.metadata.getProperty("title").getOrElse(id)
       tr(
         td(id),
-        td(a(href := id+".meta")(title))
+        td(a(href := prefix + id + ".meta")(title))
       )
-    }.compile.toList.map { catalogEntries =>
-      table(
-        caption(b(i(u("Catalog")))),
-        tr(
-          th("id"),
-          th("title"),
-        ),
-        catalogEntries
-      )
+    }.compile.toList.map { catalogDatasets =>
+      if (catalogDatasets.isEmpty) {
+        div()
+      } else {
+        table(
+          caption(i("Datasets")),
+          tr(
+            th("id"),
+            th("title")
+          ),
+          catalogDatasets
+        )
+      }
     }
+
+  /** Provides a recursive HTML table representation of a Catalog's sub-catalogs */
+  private[dap2] def subcatalogTable(catalog: Catalog, prefix: String = ""): IO[Text.TypedTag[String]] = {
+    val catalogs = Stream.emits(catalog.catalogs.toList)
+    catalogs.evalMap { c =>
+      val id = c._1.asString
+      val cat = c._2
+      for {
+        datasets <- datasetTable(cat, prefix + id + "/")
+        subcatalogs <- subcatalogTable(cat, prefix + id + "/")
+      } yield {
+        tr(
+          td(details(
+            summary(b(i(u(id))), emptyTab,
+              a(href := prefix + id + "/")(forwardArrow)
+            ),
+            div(datasets,
+              subcatalogs)
+          ))
+        )
+      }
+    }.compile.toList.map { catalogSubcatalogs =>
+      if (catalogSubcatalogs.isEmpty) {
+        div()
+      } else {
+        table(`class` := "subcatalog",
+          caption(i("Subcatalogs")),
+          catalogSubcatalogs
+        )
+      }
+    }
+  }
 }
