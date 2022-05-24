5f33a0e00897c8277789dee5c229ecb6b8e5eceb
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-05-24 17:12:51 -0600

    cleanup

--- core/src/test/scala/latis/units/UnitConverterSuite.scala
+++ core/src/test/scala/latis/units/UnitConverterSuite.scala
@@ -8,7 +8,10 @@ class UnitConverterSuite extends FunSuite {
 
   test("convert time units") {
     val ts1 = TimeScale.Default
-    val ts2 = TimeScale.fromExpression("seconds since 1970").getOrElse(fail("Time Scale not generated"))
+    val ts2 = TimeScale
+      .fromExpression("seconds since 1970")
+      .getOrElse(fail("failed to create TimeScale"))
+
     assertEquals(UnitConverter(ts1, ts2).convert(1000), 1.0)
   }
 }
