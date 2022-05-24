2ffab0b3b503a741f46688f9c6626834cfc6e625
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-05-24 16:26:08 -0600

    cleanup

--- core/src/test/scala/latis/metadata/MetadataSuite.scala
+++ core/src/test/scala/latis/metadata/MetadataSuite.scala
@@ -3,14 +3,14 @@ package latis.metadata
 import munit.FunSuite
 
 class MetadataSuite extends FunSuite {
-  
+
   /**
    * Instance of Metadata for testing.
-   * Note that Metadata extends PropertiesLike 
+   * Note that Metadata extends PropertiesLike
    * which is more thoroughly tested.
    */
   val testmd = Metadata("id" -> "testds")
-  
+
   /**
    * Instance of MetadataLike for testing.
    */
@@ -21,17 +21,17 @@ class MetadataSuite extends FunSuite {
   test("get metadata property") {
     testmd.getProperty("id") match {
       case Some(id) => assertEquals(id, "testds")
-      case _ => fail("id not generated")
+      case _ => fail("")
     }
   }
-  
+
   test("get metadata like property") {
     mlike("id") match {
       case Some(id) => assertEquals(id, "testds")
-      case _ => fail("id not generated")
+      case _ => fail("")
     }
   }
-  
+
   test("get_metadata_like_invalid_property") {
     val result = mlike("invalid")
     assert(result.isEmpty)
