57fa03d523592f16a48382d23e50c7d470f5eb36
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-07-04 14:59:38 -0600

    Migrate StringUtils tests to munit

--- core/src/test/scala/latis/util/StringUtilSuite.scala
+++ core/src/test/scala/latis/util/StringUtilSuite.scala
@@ -1,58 +1,59 @@
 package latis.util
 
-import org.scalatest.funsuite.AnyFunSuite
+import munit.FunSuite
+
 import StringUtils._
 
-class StringUtilsSuite extends AnyFunSuite {
+class StringUtilsSuite extends FunSuite {
 
   test("quote unquoted string") {
     val s = "foo" //foo
     val qs = ensureDoubleQuoted(s)
-    assert(qs == """"foo"""") //"foo"
+    assertEquals(qs, """"foo"""") //"foo"
   }
 
   test("quote quoted string") {
     val s = """"foo""""  //"foo"
     val qs = ensureDoubleQuoted(s)
-    assert(qs == """"foo"""") //"foo"
+    assertEquals(qs, """"foo"""") //"foo"
   }
 
   test("quote partially quoted string") {
     val s = """"foo""" //"foo
     val qs = ensureDoubleQuoted(s)
-    assert(qs == """"\"foo"""") //"\"foo"
+    assertEquals(qs, """"\"foo"""") //"\"foo"
   }
 
   test("single quote unquoted string") {
     val s = "foo" //foo
     val qs = ensureSingleQuoted(s)
-    assert(qs == "'foo'") //'foo'
+    assertEquals(qs, "'foo'") //'foo'
   }
 
   test("single quote quoted string") {
     val s = "'foo'"  //'foo'
     val qs = ensureSingleQuoted(s)
-    assert(qs == "'foo'") //'foo'
+    assertEquals(qs, "'foo'") //'foo'
   }
 
   test("single quote partially quoted string") {
     val s = "'foo" //'foo
     val qs = ensureSingleQuoted(s)
-    assert(qs == "'\\\'foo'") //'\'foo'
+    assertEquals(qs, "'\\\'foo'") //'\'foo'
   }
 
   test("quote empty string") {
     val qs = ensureQuoted("", 'q')
-    assert(qs == "qq")
+    assertEquals(qs, "qq")
   }
 
   test("remove double quotes") {
     val s = """"foo""""
-    assert(removeDoubleQuotes(s) == "foo")
+    assertEquals(removeDoubleQuotes(s), "foo")
   }
 
   test("remove double quotes no-op") {
     val s = "foo"
-    assert(removeDoubleQuotes(s) == "foo")
+    assertEquals(removeDoubleQuotes(s), "foo")
   }
 }
