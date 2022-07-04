7af8a52fa74edb26abf8bdd7db8d736479de1e5b
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-07-04 12:28:28 -0600

    Migrate ColumnarAdapter tests to munit

--- core/src/test/scala/latis/input/ColumnarAdapterSuite.scala
+++ core/src/test/scala/latis/input/ColumnarAdapterSuite.scala
@@ -1,41 +1,40 @@
 package latis.input
 
-import org.scalatest.EitherValues._
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers._
+import munit.FunSuite
 
 import latis.data._
 import latis.dsl.ModelParser
 import latis.metadata.Metadata
 import latis.model._
-import latis.util.Identifier.IdentifierStringContext
+import latis.util.Identifier._
 
-class ColumnarAdapterSpec extends AnyFlatSpec {
+class ColumnarAdapterSuite extends FunSuite {
 
   private lazy val config = new ColumnarAdapter.Config(
     ("delimiter", " "),
     ("columns", "0,1,2;5;3;4")
   )
+
   private lazy val model = (for {
     d <- Scalar.fromMetadata(Metadata(id"time") + ("type" -> "string") + ("units" -> "yyyy MM dd"))
     r <- ModelParser.parse("(myInt: int, myDouble: double, myString: string)")
     f <- Function.from(d, r)
-  } yield f).value
+  } yield f).fold(fail("failed to create model", _), identity)
 
   private lazy val colAdapter = new ColumnarAdapter(model, config)
 
-  "A ColumnarAdapter" should "parse a record given column indices" in {
+  test("parse a record given column indices") {
     val record         = "1970 1 1 1.1 A 1"
     val sample         = colAdapter.parseRecord(record)
     val expectedSample = Some(Sample(DomainData("1970 1 1"), RangeData(1, 1.1, "A")))
 
-    sample should be(expectedSample)
+    assertEquals(sample, expectedSample)
   }
 
-  "A ColumnarAdapter" should "ignore a record with too few values" in {
+  test("ignore a record with too few values") {
     val record = "1970 1 1 1.1 A"
     val sample = colAdapter.parseRecord(record)
 
-    sample should be(None)
+    assertEquals(sample, None)
   }
 }
