74ba34f5c869bb77f2e6c0fa9e0c4cdf9e0f020f
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2022-07-08 18:11:21 -0600

    Migrate TakeRight tests to munit

--- core/src/test/scala/latis/ops/TakeRightSuite.scala
+++ core/src/test/scala/latis/ops/TakeRightSuite.scala
@@ -1,20 +1,18 @@
 package latis.ops
 
-import cats.effect.unsafe.implicits.global
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers._
+import munit.CatsEffectSuite
 
 import latis.data._
 import latis.dataset.Dataset
 import latis.dsl._
 
-class TakeRightSpec extends AnyFlatSpec {
+class TakeRightSuite extends CatsEffectSuite {
 
-  "The TakeRight Operation" should "return the last n samples of a simple dataset" in {
+  test("return the last n samples of a simple dataset") {
     val ds: Dataset = DatasetGenerator("a -> b")
     val dsTake      = ds.withOperation(TakeRight(2))
-    val samples     = dsTake.samples.compile.toList.unsafeRunSync()
-    samples should be(
+    val samples     = dsTake.samples.compile.toList
+    samples.assertEquals(
       List(
         Sample(DomainData(1), RangeData(1)),
         Sample(DomainData(2), RangeData(2))
@@ -22,9 +20,9 @@ class TakeRightSpec extends AnyFlatSpec {
     )
   }
 
-  it should "return the last n samples of a dataset with a nested function" in {
+  test("return the last n samples of a dataset with a nested function") {
     val ds      = DatasetGenerator("(a, b) -> c").curry(1).withOperation(TakeRight(1))
-    val samples = ds.samples.compile.toList.unsafeRunSync()
+    val samples = ds.samples.compile.toList
     val sf = SampledFunction(
       Seq(
         Sample(DomainData(0), RangeData(3)),
@@ -32,6 +30,6 @@ class TakeRightSpec extends AnyFlatSpec {
         Sample(DomainData(2), RangeData(5))
       )
     )
-    samples should be(List(Sample(DomainData(1), Seq(sf))))
+    samples.assertEquals(List(Sample(DomainData(1), Seq(sf))))
   }
 }
