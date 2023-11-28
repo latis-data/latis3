221dcc109b9a81d981849c3069c81cff2315dd0b
Doug Lindholm <dlindhol@gmail.com>
2023-11-28 10:41:52 -0700

    Add method to remove Metadata property

--- core/src/test/scala/latis/metadata/MetadataSuite.scala
+++ core/src/test/scala/latis/metadata/MetadataSuite.scala
@@ -9,13 +9,13 @@ class MetadataSuite extends FunSuite {
    * Note that Metadata extends PropertiesLike
    * which is more thoroughly tested.
    */
-  val testmd = Metadata("id" -> "testds")
+  val testmd: Metadata = Metadata("id" -> "testds")
 
   /**
    * Instance of MetadataLike for testing.
    */
-  val mlike = new MetadataLike {
-    def metadata = testmd
+  val mlike: MetadataLike = new MetadataLike {
+    def metadata: Metadata = testmd
   }
 
   test("get metadata property") {
@@ -35,5 +35,27 @@ class MetadataSuite extends FunSuite {
   test("get_metadata_like_invalid_property") {
     val result = mlike("invalid")
     assert(result.isEmpty)
+  }
+
+  test("add property") {
+    val md = testmd + ("foo", "bar")
+    val value = md.getProperty("foo").getOrElse(fail("foo property not found"))
+    assertEquals(value, "bar")
+  }
+
+  test("replace property") {
+    val md = testmd + ("foo", "bar") + ("foo", "baz")
+    val value = md.getProperty("foo").getOrElse(fail("foo property not found"))
+    assertEquals(value, "baz")
+  }
+
+  test("combine metadata with override") {
+    val md1 = Metadata("id" -> "md1") + ("foo", "bar")
+    val md2 = Metadata("name" -> "md2") + ("foo", "baz")
+    val md = md1 ++ md2
+    assertEquals(md.properties.size, 3)
+    assertEquals(md.getProperty("id").getOrElse(fail("property not found")), "md1")
+    assertEquals(md.getProperty("name").getOrElse(fail("property not found")), "md2")
+    assertEquals(md.getProperty("foo").getOrElse(fail("property not found")), "baz")
   }
 }
