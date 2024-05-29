b4788d1b03a43ce8d5c0b8f73d06e37e66a17c3f
Chris Lindholm <chris.lindholm@gmail.com>
2024-05-29 09:06:04 -0600

    Add SseEncoder

---
+++ core/src/main/scala/latis/output/SseEncoder.scala
@@ -0,0 +1,45 @@
+package latis.output
+
+import cats.effect.IO
+import cats.syntax.all.*
+import fs2.Chunk
+import fs2.Stream
+import io.circe.syntax.*
+import org.http4s.ServerSentEvent
+
+import latis.data.Sample
+import latis.dataset.Dataset
+import latis.metadata.Metadata
+import latis.util.LatisException
+
+class SseEncoder extends Encoder[IO, ServerSentEvent] {
+
+  override def encode(dataset: Dataset): Stream[IO, ServerSentEvent] =
+    Stream.emit(makeMetadataEvent(dataset.metadata)) ++
+      Stream.emit(makeInfoEvent("here comes the data")) ++
+      dataset.samples.mapChunks(makeDataEvent(_).pure[Chunk]).handleErrorWith {
+        case e: LatisException =>
+          // NOTE: Making the assumption that LatisException messages
+          // are safe to share with users.
+          Stream.emit(makeErrorEvent(e.message))
+        case _ => Stream.emit(makeErrorEvent("unspecified error"))
+      }
+
+  private def makeDataEvent(samples: Chunk[Sample]): ServerSentEvent =
+    ServerSentEvent(
+      samples.toList.asJson.noSpaces.some,
+      "data".some
+    )
+
+  private def makeErrorEvent(message: String): ServerSentEvent =
+    ServerSentEvent(message.some, "error".some)
+
+  private def makeInfoEvent(message: String): ServerSentEvent =
+    ServerSentEvent(message.some, "info".some)
+
+  private def makeMetadataEvent(metadata: Metadata): ServerSentEvent =
+    ServerSentEvent(
+      metadata.properties.asJson.noSpaces.some,
+      "metadata".some
+    )
+}
