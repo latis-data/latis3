4843e1d4b84a3bae5b00b82048958ea6f1d5a07c
Chris Lindholm <chris.lindholm@gmail.com>
2025-07-01 19:51:32 -0600

    Replace URL literals in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "LaTiS development environment";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
