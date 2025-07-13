4c913bc45fed1a37d2b009d23e0501aeb7f749d9
Doug Lindholm <dlindhol@gmail.com>
2025-07-13 14:13:10 -0600

    Add the ability to constrain a Bounds

--- core/src/main/scala/latis/util/Bounds.scala
+++ core/src/main/scala/latis/util/Bounds.scala
@@ -23,6 +23,34 @@ final case class Bounds[T] private (val lower: T, val upper: T)(ord: Ordering[T]
     // NaN greater than other values but the less than test will be false.
     ord.gteq(value, lower) && (ord.lt(value, upper) || (inclusive && ord.equiv(value, upper)))
   }
+
+  /**
+   * Constrains the lower bound.
+   *
+   * This optionally returns a new Bounds if the given value is greater than
+   * the current lower bound. Otherwise, it returns itself. If the bounds
+   * become invalid: lower > upper, this will return None.
+   *
+   * @param value the potentially new lower bound
+   */
+  def constrainLower(value: T): Option[Bounds[T]] = {
+    if (ord.gt(value, lower)) Bounds.of(value, upper)(ord) //TODO: why can't we use ">"?
+    else Some(this)
+  }
+
+  /**
+   * Constrains the upper bound.
+   *
+   * This optionally returns a new Bounds if the given value is less than the
+   * current upper bound. Otherwise, it returns itself. If the bounds become
+   * invalid: lower > upper, this will return None.
+   *
+   * @param value the potentially new upper bound
+   */
+  def constrainUpper(value: T): Option[Bounds[T]] = {
+    if (ord.lt(value, upper)) Bounds.of(lower, value)(ord)
+    else Some(this)
+  }
 }
 
 object Bounds {
