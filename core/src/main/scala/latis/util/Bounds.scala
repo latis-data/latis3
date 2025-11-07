8508a4e6136a2f2dab28138d16380eb1a60e9f6c
Doug Lindholm <dlindhol@gmail.com>
2025-11-07 11:43:24 -0700

    Improve Bounds
    This handles inclusivity and the concept of empty bounds. The handling of bin semantics needed updating. This will break other uses of the old Bounds so we need to be careful about updating latis3 in other projects.

--- core/src/main/scala/latis/util/Bounds.scala
+++ core/src/main/scala/latis/util/Bounds.scala
@@ -1,68 +1,150 @@
 package latis.util
 
 /**
- * [[Bounds]] represents the lower and upper bounds of a range.
+ * [[Bounds]] represent the lower and upper extents of a coverage.
  *
- * It enforces that the upper bound is strictly greater than
- * the lower bound.
+ * Note that "lower" and "upper" bounds are compared based on an
+ * Ordering, not necessarily the usual numeric ordering.
  */
-final case class Bounds[T] private (lower: T, upper: T)(ord: Ordering[T]) {
+sealed trait Bounds[T] {
+
+  /** Is the given value covered by this [[Bounds]] */
+  def contains(value: T): Boolean
+
+  /** Does this [[Bounds]] have a zero coverage */
+  def isEmpty: Boolean
 
   /**
-   * Tests whether the given value falls within this [[Bounds]].
-   *
-   * The lower bound is always inclusive but the upper bound defaults
-   * to being exclusive. NaN is never within bounds.
-   *
-   * @param value the value being tested
-   * @param inclusive specifies if the upper bound is inclusive,
-   *                  default is false
+   * Replaces the lower bound if the given value further constrains it.
+   * If the upper bound does not cover the value, this will return an
+   * empty [[Bounds]].
    */
-  def contains(value: T, inclusive: Boolean = false): Boolean = {
-    //Note that the default Double.TotalOrdering (unlike IeeeOrdering) will treat
-    // NaN greater than other values but the less than test will be false.
-    ord.gteq(value, lower) && (ord.lt(value, upper) || (inclusive && ord.equiv(value, upper)))
-  }
+  def constrainLower(value: T): Bounds[T]
 
   /**
-   * Constrains the lower bound.
-   *
-   * This optionally returns a new Bounds if the given value is greater than
-   * the current lower bound. Otherwise, it returns itself. If the bounds
-   * become invalid: lower > upper, this will return None.
-   *
-   * @param value the potentially new lower bound
+   * Replaces the upper bound if the given value further constrains it.
+   * If the lower bound does not cover the value, this will return an
+   * empty [[Bounds]].
    */
-  def constrainLower(value: T): Option[Bounds[T]] = {
-    if (ord.gt(value, lower)) Bounds.of(value, upper)(ord)
-    else Some(this)
+  def constrainUpper(value: T): Bounds[T]
+}
+
+/**
+ * [[NonEmptyBounds]] represent a coverage with an upper [[Bound]]
+ * that is greater than the lower [[Bound]].
+ */
+case class NonEmptyBounds[T] private[util] (lower: Bound[T], upper: Bound[T])
+  (using ord: Ordering[T]) extends Bounds[T] {
+
+  def contains(value: T): Boolean =
+    lower.boundsLower(value) && upper.boundsUpper(value)
+
+  def isEmpty: Boolean = false
+
+  def constrainLower(value: T): Bounds[T] = {
+    if (lower.boundsLower(value)) {
+      if (upper.boundsUpper(value))
+        NonEmptyBounds(lower.updated(value), upper)
+      else Bounds.empty
+    }
+    else this
   }
 
-  /**
-   * Constrains the upper bound.
-   *
-   * This optionally returns a new Bounds if the given value is less than the
-   * current upper bound. Otherwise, it returns itself. If the bounds become
-   * invalid: lower > upper, this will return None.
-   *
-   * @param value the potentially new upper bound
-   */
-  def constrainUpper(value: T): Option[Bounds[T]] = {
-    if (ord.lt(value, upper)) Bounds.of(lower, value)(ord)
-    else Some(this)
+  def constrainUpper(value: T): Bounds[T] = {
+    if (upper.boundsUpper(value)) {
+      if (lower.boundsLower(value))
+        NonEmptyBounds(lower, upper.updated(value))
+      else Bounds.empty
+    }
+    else this
   }
+
+  override def toString: String = (lower, upper) match {
+    case (ClosedBound(l), ClosedBound(u)) => s"[$l,$u]"
+    case (ClosedBound(l), OpenBound(u))   => s"[$l,$u)"
+    case (OpenBound(l), ClosedBound(u))   => s"($l,$u]"
+    case (OpenBound(l), OpenBound(u))     => s"($l,$u)"
+  }
+}
+
+/** [[Bounds]] that contain a single value */
+case class PointBounds[T](point: T) extends Bounds[T] {
+  def contains(value: T): Boolean = point == value
+  def isEmpty: Boolean = false
+  def constrainLower(value: T): Bounds[T] =
+    if (point == value) this else Bounds.empty
+  def constrainUpper(value: T): Bounds[T] =
+    if (point == value) this else Bounds.empty
+  override def toString: String = s"[$point]"
+}
+
+/** [[Bounds]] that cover nothing */
+class EmptyBounds[T] extends Bounds[T] {
+  def contains(value: T): Boolean = false
+  def isEmpty: Boolean = true
+  def constrainLower(value: T): Bounds[T] = Bounds.empty
+  def constrainUpper(value: T): Bounds[T] = Bounds.empty
+  override def toString: String = "0"
 }
 
 object Bounds {
 
-  /** Tries to construct a [[Bounds]] from lower and upper values. */
-  def of[T](lower: T, upper: T)(implicit ord: Ordering[T]): Option[Bounds[T]] =
+  def empty[T]: Bounds[T] = new EmptyBounds
+
+  /**
+   * Constructs [[Bounds]] with an inclusive lower bound and
+   * exclusive upper bound.
+   *
+   * If either bound is a NaN, this will return an empty [[Bounds]].
+   */
+  def of[T](lower: T, upper: T)(using ord: Ordering[T]): Bounds[T] =
+    //Need special handling for NaN
     (lower, upper) match {
-      //Need special handling for NaN since default TotalOrdering.lt(_, NaN) is true
-      case (_, u: Double) if (u.isNaN) => None
-      case (_, u: Float)  if (u.isNaN) => None
-      case (l, u) if (ord.lt(l, u)) => Some(new Bounds[T](lower, upper)(ord))
-      case _ => None
+      case (l: Double, u: Double) if (l.isNaN || u.isNaN) => Bounds.empty
+      case (l: Float, u: Float)   if (l.isNaN || u.isNaN) => Bounds.empty
+      case (l, u) if (ord.lt(lower, upper)) =>
+        NonEmptyBounds(ClosedBound(lower), OpenBound(upper))
+      case _ => Bounds.empty
     }
 
+  /**
+   * Constructs [[Bounds]] with inclusive lower and upper bounds.
+   */
+  def inclusive[T](lower: T, upper: T)(using ord: Ordering[T]): Bounds[T] = {
+    if (ord.lt(lower, upper))
+      NonEmptyBounds(ClosedBound(lower), ClosedBound(upper))
+    else if (lower == upper) PointBounds(lower)
+    else Bounds.empty
+  }
+}
+
+
+/**
+ * [[Bound]] defines a boundary (lower or upper) of a coverage.
+ */
+sealed trait Bound[T] {
+  /** Returns the bounding value */
+  def value: T
+  /** Does this bound the given value on the lower side */
+  def boundsLower(value: T): Boolean
+  /** Does this bound the given value on the upper side */
+  def boundsUpper(value: T): Boolean
+  /** Returns a new Bound with the updated value */
+  def updated(value: T): Bound[T]
+}
+
+/** Defines a [[Bound]] that is inclusive. */
+final case class ClosedBound[T](val value: T)
+(using ord: Ordering[T]) extends Bound[T] {
+  def boundsLower(v: T): Boolean = ord.lteq(value, v)
+  def boundsUpper(v: T): Boolean = ord.gteq(value, v)
+  def updated(v: T): Bound[T] = ClosedBound[T](v)
+}
+
+/** Defines a [[Bound]] that is exclusive. */
+final case class OpenBound[T](val value: T)
+(using ord: Ordering[T]) extends Bound[T] {
+  def boundsLower(v: T): Boolean = ord.lt(value, v)
+  def boundsUpper(v: T): Boolean = ord.gt(value, v)
+  def updated(v: T): Bound[T] = OpenBound[T](v)
 }
