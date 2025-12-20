2d3b0401b26100508a81b6156c13e1b2a6d4f37e
Doug Lindholm <dlindhol@gmail.com>
2025-12-20 14:08:46 -0700

    Add OnChange operation

---
+++ core/src/test/scala/latis/ops/OnChangeSuite.scala
@@ -0,0 +1,80 @@
+package latis.ops
+
+import munit.CatsEffectSuite
+
+import latis.data.DomainData
+import latis.data.RangeData
+import latis.data.Sample
+import latis.data.SampledFunction
+import latis.dataset.MemoizedDataset
+import latis.dsl.*
+import latis.metadata.Metadata
+import latis.util.Identifier.id
+
+class OnChangeSuite extends CatsEffectSuite {
+
+  private val model = ModelParser.parse("x -> y").fold(throw _, identity)
+
+  // samples with some non-changing range values
+  private val samples = List(
+    Sample(DomainData(0), RangeData(0)),
+    Sample(DomainData(1), RangeData(0)),
+    Sample(DomainData(2), RangeData(0)),
+    Sample(DomainData(3), RangeData(1)),
+    Sample(DomainData(4), RangeData(1)),
+    Sample(DomainData(5), RangeData(1)),
+  )
+
+  private val ds = new MemoizedDataset(
+    Metadata(id"test"),
+    model,
+    SampledFunction(samples)
+  )
+
+  test("on change") {
+    val oc = OnChange.fromArgs(List("y"))
+      .fold(_ => fail("Failed to make OnChange"), identity)
+    val samples = ds.withOperation(oc).samples.compile.toList
+    samples.assertEquals(
+      List(
+        Sample(DomainData(0), RangeData(0)),
+        Sample(DomainData(3), RangeData(1)),
+        Sample(DomainData(5), RangeData(1)), //keep last sample to preserve coverage
+      )
+    )
+  }
+
+  test("last sample changes") {
+    val samples = List(
+      Sample(DomainData(0), RangeData(0)),
+      Sample(DomainData(1), RangeData(1)),
+      Sample(DomainData(2), RangeData(2))
+    )
+
+    val ds = new MemoizedDataset(
+      Metadata(id"test"),
+      model,
+      SampledFunction(samples)
+    )
+    ds.samples.compile.toList.assertEquals(
+      List(
+        Sample(DomainData(0), RangeData(0)),
+        Sample(DomainData(1), RangeData(1)),
+        Sample(DomainData(2), RangeData(2))
+      )
+    )
+  }
+
+  test("duplicate domain value") {
+    val oc = OnChange.fromArgs(List("x"))
+      .fold(_ => fail("Failed to make OnChange"), identity)
+    val ds = MemoizedDataset(
+      Metadata(id"test"),
+      model,
+      SampledFunction(samples :+ Sample(DomainData(5), RangeData(2)))
+    )
+    ds.withOperation(oc).samples.compile.toList.map(_.length)
+      .assertEquals(7)
+  }
+
+}
