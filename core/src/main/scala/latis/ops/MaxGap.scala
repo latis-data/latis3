0855d87338a84d3675e36474f2c5680686b19925
Doug Lindholm <dlindhol@gmail.com>
2025-12-20 14:10:55 -0700

    Add operation to fill gaps

--- core/src/main/scala/latis/ops/MaxGap.scala
+++ core/src/main/scala/latis/ops/MaxGap.scala
@@ -9,8 +9,8 @@ import latis.model.*
 import latis.util.LatisException
 
 /**
- * Inserts Samples such that the domain values have no gap greater 
- * than the given [[gapSize]], using the given Interpolation algorithm. 
+ * Inserts Samples such that the domain values have no gap greater
+ * than the given [[gapSize]], using the given Interpolation algorithm.
  * The resulting cadence will be the gap size.
  *
  * Requires an arity-1 Function with a numeric domain variable, and
@@ -83,11 +83,11 @@ object MaxGap {
       } yield MaxGap(value, interp)
     case _ => LatisException("MaxGap requires a gap size and interpolation").asLeft
   }
-  
+
   private def parseGapSize(s: String): Either[LatisException, Double] = {
     //TODO: support ISO 8601 duration, need model to get units right
     //if (s.startsWith("P")) Either.catchNonFatal {
-    //  Duration.parse(s).toMillis.toDouble 
+    //  Duration.parse(s).toMillis.toDouble
     //} else {
       s.toDoubleOption.flatMap {
         case d if (d > 0) => d.some
@@ -95,8 +95,8 @@ object MaxGap {
       }.toRight(LatisException("Gap size must be a positive number"))
     //}
   }
-  
-  private def parseInterpolation(s: String): Either[LatisException, Interpolation] = 
+
+  private def parseInterpolation(s: String): Either[LatisException, Interpolation] =
     Interpolation.fromName(s).flatMap {
       case NoInterpolation() => LatisException("MaxGap requires interpolation").asLeft
       case interp => interp.asRight
