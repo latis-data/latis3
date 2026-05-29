b8413a5fc9d37b2e31e96cc3d971eb41303682ce
Doug Lindholm <dlindhol@gmail.com>
2026-05-29 12:52:59 -0600

    join snapshot

--- core/src/main/scala/latis/ops/VerticalJoin.scala
+++ core/src/main/scala/latis/ops/VerticalJoin.scala
@@ -2,23 +2,27 @@ package latis.ops
 
 import cats.syntax.all.*
 
-import latis.model.DataType
+import latis.model.*
 import latis.util.LatisException
 
 /**
  * Trait for Joins that combine Datasets "vertically".
  *
  * This Join is vertical in the sense that it combines
- * like Datasets along the domain dimensions as opposed
- * to a HorizontalJoin which can combine unlike range
- * variables. A VerticalJoin requires that each dataset
- * and the result has the same model.
+ * like Datasets along the domain dimensions (e.g. appending
+ * time series). A VerticalJoin requires that each dataset
+ * has the same (or compatible) model. The resulting dataset
+ * will have the model of the first dataset.
  */
 trait VerticalJoin extends Join {
   //TODO: validate that models are the same, compatible?
 
   // Model is the same and unchanged, use the first
   def applyToModel(model1: DataType, model2: DataType):
-    Either[LatisException, DataType] = model1.asRight
+    Either[LatisException, DataType] = {
+    // Combine Scalars or Tuples as Function of Index //TODO: test
+    if (model1.arity == 0) Function.from(Index(), model1)
+    else model1.asRight
+  }
 
 }
